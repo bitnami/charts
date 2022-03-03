27e1f3c338be457263412492a107897a6e6b6901
alukic <alukic@vmware.com>
2022-03-03 10:07:09 +0100

    Randomised test data, added changes suggested in the code review
    Signed-off-by: alukic <alukic@vmware.com>

--- .vib/grafana/cypress/cypress/integration/grafana_test_suite.js
+++ .vib/grafana/cypress/cypress/integration/grafana_test_suite.js
@@ -1,8 +1,6 @@
 /// <reference types="cypress" />
 import {
-  deleteDashboard,
-  deleteAPIKey,
-  clickDeleteButton,
+  random,
   verifySuccesOfAction
 } from './utils'
 
@@ -14,60 +12,70 @@ it('allows user to log out', () => {
 
 it('allows creating a dashboard with a panel', () => {
   const CLOSE_DIALOGUE_BUTTON = 'button[aria-label="Close dialogue"]';
+  const PANEL_TITLE = 'Test panel title';
+  const PANEL_DESCRIPTION = 'Test panel description';
+  const DASHBOARD_TITLE = 'Dashboard title';
 
   cy.login();
   cy.visit('dashboard/new');
   cy.get('button').contains('Add a new panel')
     .should('be.visible').click();
-  cy.get('[value="Panel Title"]').clear().type(Cypress.env('panel_title'));
+  cy.get('[value="Panel Title"]').clear().type(`${PANEL_TITLE}.${random}`);
   cy.get('textarea#description-text-area')
-    .should('exist').type(Cypress.env('panel_description'));
+    .should('exist').type(`${PANEL_DESCRIPTION}.${random}`);
   cy.get('button').contains('Apply')
     .should('be.visible').click();
-  cy.get('.panel-title').contains(Cypress.env('panel_title'));
+  cy.get('.panel-title').contains(PANEL_TITLE).should('be.visible');
   cy.get('[aria-label="Save dashboard"]').click();
   cy.get(CLOSE_DIALOGUE_BUTTON)
     .should('be.visible');
-  cy.get('[name="title"]').clear().type(Cypress.env('dashboard_title'));
+  cy.get('[name="title"]').clear().type(`${DASHBOARD_TITLE}.${random}`);
   cy.get('[type="submit"]')
     .should('be.visible').click();
   verifySuccesOfAction();
-  deleteDashboard();
 })
 
 it('checks if it is possible to upload a dashboard as JSON file', () => {
   const IMPORT_DASHBOARD_BUTTON = '[data-testid="data-testid-import-dashboard-submit"]';
+  const DASHBOARD_TITLE = "New Test dashboard";
   
   cy.login();
   cy.visit("dashboard/import");
   cy.get('label').contains('Upload JSON').click();
   cy.get('input[type=file]').selectFile('cypress/fixtures/test-dashboard.json',
   {force:true});
+  cy.get('[data-testid="data-testid-import-dashboard-title"]').clear().type(`${DASHBOARD_TITLE}.${random}`);
   cy.get(IMPORT_DASHBOARD_BUTTON).click();
   cy.visit("dashboards");
-  cy.get('div').contains('New test dashboard')
+  cy.get('div').contains(DASHBOARD_TITLE)
     .should('exist').click();
-  deleteDashboard();
 })
 
 it('allows inviting a user', () => {
+  const TEST_EMAIL_ADDRESS = 'test@email.com';
+  const INVITE_USER_NAME = 'Invited test user';
+
   cy.login();
   cy.visit('/org/users/invite');
-  cy.get('[name="loginOrEmail"]').clear().type(Cypress.env('test_email_address'));
-  cy.get('[name="name"]').clear().type(Cypress.env('invite_user_name'));
+  cy.get('[name="loginOrEmail"]').clear().type(`${TEST_EMAIL_ADDRESS}.${random}`);
+  cy.get('[name="name"]').clear().type(`${INVITE_USER_NAME}.${random}`);
   cy.get('[name="sendEmail"]').click({force: true});
   cy.get('[type="submit"]').click();
   verifySuccesOfAction();
 })
 
 it('checks if smtp is configured', () => {
+  const SMTP_ADDRESS = 'test@smtp.com';
+  const SMTP_NAME = 'test_smtp';
+  const SMTP_USER = 'smtp_user';
+
   cy.login();
   cy.visit("admin/settings");
-  cy.get('td').contains(Cypress.env('smtp_address'))
+  cy.get('td').contains(SMTP_ADDRESS)
     .should('exist');
-  cy.get('td').contains(Cypress.env('smtp_name'))
+  cy.get('td').contains(SMTP_NAME)
     .should('exist');
-  cy.get('td').contains(Cypress.env('smtp_user'))
+  cy.get('td').contains(SMTP_USER)
     .should('exist');
 })
 
@@ -81,6 +89,7 @@ it('checks if plugin page is showing plugins', () => {
 it('checks if it is possible to create and delete a data source', () => {
   const DATASOURCE = 'Prometheus';
   const DATASOURCE_ID = 'button[aria-label= "Add data source ' + DATASOURCE + '"]'
+  const DELETE_BUTTON = 'button[aria-label="Confirm Modal Danger Button"]';
 
   cy.login();
   cy.visit("/datasources/new");
@@ -94,22 +103,27 @@ it('checks if it is possible to create and delete a data source', () => {
   cy.get('button').contains('Delete').click();
   cy.get('div').contains('Are you sure you want to delete')
     .should('be.visible');
-  clickDeleteButton();
+  cy.get(DELETE_BUTTON).should('be.visible').click();
   verifySuccesOfAction();
 })
 
-it('checks if an API key can be added', () => {
+it('checks if an API key can be added and deleted', () => {
   const ADD_NEW_API_KEY = '[data-testid="data-testid Call to action button New API key"]';
+  const API_KEY_NAME = "test_api_key";
+  const CLOSE_DIALOGUE_BUTTON = 'button[aria-label="Close dialogue"]';
+  const DELETE_API_KEY_BUTTON = 'button[aria-label="Delete API key"]';
 
   cy.login();
   cy.visit('org/apikeys');
   cy.get(ADD_NEW_API_KEY).click();
   cy.get('input[placeholder="Name"]')
-    .should('be.visible').type("test")
+    .should('be.visible').type(`${API_KEY_NAME}.${random}`)
   cy.get('button').contains('Add').click({force:true});
   cy.get('h2').contains('API Key Created')
     .should('be.visible');
-  deleteAPIKey();
+  cy.get(CLOSE_DIALOGUE_BUTTON).click();
+  cy.get(DELETE_API_KEY_BUTTON).click();
+  cy.get('span').contains('Delete').click();
 })
 
 it('checks admin settings endpoint',() => {
