e77ccccd53dfed5dcded0eacd729294fefbb4ce6
alukic <alukic@vmware.com>
2022-03-08 18:03:11 +0100

    Added improvement of potentially flaky test, added randomiser
    Signed-off-by: alukic <alukic@vmware.com>

--- .vib/wordpress/cypress/cypress/support/commands.js
+++ .vib/wordpress/cypress/cypress/support/commands.js
@@ -1,26 +1,24 @@
-// Added to slow down Cypress test execution without using hardcoded waits. If removed, there will be false positives. 
-
-const COMMAND_DELAY = 200;
+const COMMAND_DELAY = 500;
 
 for (const command of ['click']) {
-  Cypress.Commands.overwrite(command, (originalFn, ...args) => {
-    const origVal = originalFn(...args);
+    Cypress.Commands.overwrite(command, (originalFn, ...args) => {
+        const origVal = originalFn(...args);
 
-    return new Promise((resolve) => {
-      setTimeout(() => {
-        resolve(origVal);
-      }, COMMAND_DELAY);
+        return new Promise((resolve) => {
+            setTimeout(() => {
+                resolve(origVal);
+            }, COMMAND_DELAY);
+        });
     });
-  });
 }
 
 Cypress.Commands.add("login", (
-  username = Cypress.env("username"),
-  password = Cypress.env("password")
+    username = Cypress.env("username"),
+    password = Cypress.env("password")
 ) => {
-  cy.clearCookies();
-  cy.visit('/wp-login.php')
-  cy.get('#user_login').type(username);
-  cy.get('#user_pass').type(password);
-  cy.get('#wp-submit').click();
+    cy.clearCookies();
+    cy.visit('/wp-login.php')
+    cy.get('#user_login').should('not.be.disabled').type(username);
+    cy.get('#user_pass').should('not.be.disabled').type(password);
+    cy.get('#wp-submit').should('be.visible').click();
 });
