3aca31f6efc6d1e2b0b24b1541bd421d62fe8733
alukic <alukic@vmware.com>
2022-03-09 09:51:06 +0100

    Removed extra spaces
    Signed-off-by: alukic <alukic@vmware.com>

--- .vib/wordpress/cypress/cypress/support/commands.js
+++ .vib/wordpress/cypress/cypress/support/commands.js
@@ -1,24 +1,24 @@
 const COMMAND_DELAY = 500;
 
 for (const command of ['click']) {
-    Cypress.Commands.overwrite(command, (originalFn, ...args) => {
-        const origVal = originalFn(...args);
+  Cypress.Commands.overwrite(command, (originalFn, ...args) => {
+      const origVal = originalFn(...args);
 
-        return new Promise((resolve) => {
-            setTimeout(() => {
-                resolve(origVal);
-            }, COMMAND_DELAY);
+      return new Promise((resolve) => {
+          setTimeout(() => {
+              resolve(origVal);
+           }, COMMAND_DELAY);
         });
     });
 }
 
 Cypress.Commands.add("login", (
-    username = Cypress.env("username"),
-    password = Cypress.env("password")
+  username = Cypress.env("username"),
+  password = Cypress.env("password")
 ) => {
-    cy.clearCookies();
-    cy.visit('/wp-login.php')
-    cy.get('#user_login').should('not.be.disabled').type(username);
-    cy.get('#user_pass').should('not.be.disabled').type(password);
-    cy.get('#wp-submit').should('be.visible').click();
+  cy.clearCookies();
+  cy.visit('/wp-login.php')
+  cy.get('#user_login').should('not.be.disabled').type(username);
+  cy.get('#user_pass').should('not.be.disabled').type(password);
+  cy.get('#wp-submit').should('be.visible').click();
 });
