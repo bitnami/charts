44b8bc57b1a504d2fdd0f0d82e862f4167c57c0c
alukic <alukic@vmware.com>
2022-03-09 09:52:01 +0100

    Removed one space more
    Signed-off-by: alukic <alukic@vmware.com>

--- .vib/wordpress/cypress/cypress/support/commands.js
+++ .vib/wordpress/cypress/cypress/support/commands.js
@@ -2,7 +2,7 @@ const COMMAND_DELAY = 500;
 
 for (const command of ['click']) {
   Cypress.Commands.overwrite(command, (originalFn, ...args) => {
-      const origVal = originalFn(...args);
+    const origVal = originalFn(...args);
 
       return new Promise((resolve) => {
           setTimeout(() => {
