b122b7449c2ec98b07208488ba66bcb0c42177f1
alukic <alukic@vmware.com>
2022-03-10 17:05:05 +0100

    Added changes per PR
    Signed-off-by: alukic <alukic@vmware.com>

--- .vib/minio/cypress/cypress/integration/minio_test_suite.js
+++ .vib/minio/cypress/cypress/integration/minio_test_suite.js
@@ -6,15 +6,15 @@ import {
 it('allows user to log in and log out', () => {
   cy.login();
   cy.get('[data-testid="ErrorOutlineIcon"]').should('not.exist');
-  cy.get('#logout').should('exist').click();
+  cy.get("#logout").scrollIntoView().should('be.visible').click();
 })
 
-it('allows creating a bucket and file upload', () => {
-  const BUCKET_TITLE = 'testbucket';
-  
+it('allows creating a bucket and file upload', () => {  
   cy.login();
   cy.visit('add-bucket');
-  cy.get('#bucket-name').should('be.visible').type(`${BUCKET_TITLE}.${random}`);
+  cy.fixture('testdata').then((td) => {
+    cy.get('#bucket-name').should('be.visible').type(`${td.bucketTitle}.${random}`);
+  })
   cy.contains('button[type="submit"]','Create Bucket').click();
   cy.contains('#upload-main','Upload').should('be.visible').click();
   cy.contains('span','Upload File').should('be.visible').click();
@@ -23,28 +23,30 @@ it('allows creating a bucket and file upload', () => {
 })
 
 it('allows creating a user', () => {
-  const TEST_ACCESS_KEY = 'Test Access Key';
-  const TEST_SECRET_KEY = 'Secret key';
-
   cy.login();
   cy.visit('identity/users');
   cy.contains('Create User');
-  cy.get('[aria-label="Create User"]').should('exist').click();
-  cy.get('#accesskey-input').should('be.visible').type(`${TEST_ACCESS_KEY}.${random}`);
-  cy.get('#standard-multiline-static').should('be.visible').type(`${TEST_SECRET_KEY}.${random}`);
+  cy.get('[aria-label="Create User"]').should('be.visible').click();
+  cy.fixture('testdata').then((td) => {
+    cy.get('#accesskey-input').should('be.visible').type(`${td.testAccessKey}.${random}`);
+    cy.get('#standard-multiline-static').should('be.visible').type(`${td.testSecretKey}.${random}`);
+  })
   cy.contains('button[type="submit"]','Save').should('be.visible').click();
-  cy.contains(TEST_ACCESS_KEY).should('be.visible');
+  cy.fixture('testdata').then((td) => {
+    cy.contains(td.testAccessKey).should('be.visible');
+    cy.get('#accesskey-input').should('not.exist');
+  })
 })
 
 it('allows creating a group', () => {
-  const TEST_GROUP_NAME = 'Test group';
-
   cy.login();
   cy.visit('identity/groups');
   cy.get('[aria-label="Create Group"]').should('be.visible').click();
-  cy.get('#group-name').should('be.visible').type(`${TEST_GROUP_NAME}.${random}`);
-  cy.contains('button[type="submit"]','Save').click();
-  cy.contains(TEST_GROUP_NAME);
+  cy.fixture('testdata').then((td) => {
+    cy.get('#group-name').should('be.visible').type(`${td.testGroupName}.${random}`);
+    cy.contains('button[type="submit"]','Save').click();
+    cy.contains(td.testGroupName);
+  })
 })
 
 it('allows creating a service account and downloading credentials', () => {
