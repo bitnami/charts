9ed6feab8ef2f6cae2ac4570bdaff18dafd3f131
alukic <alukic@vmware.com>
2022-03-29 11:15:26 +0200

    Added more functional tests which also check for existing store
    Signed-off-by: alukic <alukic@vmware.com>

--- .vib/thanos/cypress/cypress/cypress/integration/thanos_spec.js
+++ .vib/thanos/cypress/cypress/cypress/integration/thanos_spec.js
@@ -1,39 +1,43 @@
 /// <reference types="cypress" />
 
-import {
-    random,
-  } from './utils'
-  
-  it('allows to see Runtime & Build information', () => {
-    cy.visit('/');
-    cy.contains('.dropdown-toggle','Status').should('be.visible').click();
-    cy.contains('[class="dropdown-item"]','Runtime & Build Information').click();
-    cy.contains('Runtime Information').should('be.visible');
-    cy.get('h2').contains('Build Information').should('be.visible');
-  })
+it('allows to see runtime & build information', () => {
+  cy.visit('/');
+  cy.contains('.dropdown-toggle', 'Status').should('be.visible').click();
+  cy.contains('[class="dropdown-item"]', 'Runtime & Build Information').click();
+  cy.contains('Runtime Information').should('be.visible');
+  cy.get('h2').contains('Build Information').should('be.visible');
+})
 
-  it('allows the execution of a query', () => {
-    cy.visit('/');
-    cy.get('.cm-line').type('scalar {enter}');
-    cy.get('.alert').contains('Empty query result').should('be.visible');
-  })
+it('allows the execution of a query', () => {
+  cy.visit('/graph');
+  cy.get('.alert').contains('No data queried yet').should('be.visible');
+  cy.get('.cm-line').type('scalar');
+  cy.contains('.cm-completionLabel', 'scalar').should('exist').click();
+  cy.get('.execute-btn').click();
+  cy.get('.alert').contains('Empty query result').should('be.visible');
+})
 
-  it('allows listing all installed stores', () => {
-    cy.visit('/');
-    cy.contains('[class="nav-link"]','Stores').should('be.visible').click();
-    cy.contains('No stores registered').should('be.visible');
-  })
+it('allows listing all installed stores', () => {
+  cy.visit('/');
+  cy.contains('[class="nav-link"]', 'Stores').should('be.visible').click();
+  cy.get('[data-testid="endpoint"]').should('be.visible');
+  cy.get('[data-testid="health"]').should('be.visible');
+})
 
-  it('allows changing of the UI' ,() => {
-    cy.visit('/');
-    cy.contains('[class="nav-link"]','Classic UI').should('be.visible').click();
-    cy.contains('.nav-link','New UI').should('exist'); //check if the UI is switched
-  })
+it('allows changing of the UI', () => {
+  cy.visit('/');
+  cy.contains('[class="nav-link"]', 'Classic UI').should('be.visible').click();
+  cy.contains('.nav-link', 'New UI').should('exist');
+})
 
-  it('allows adding a graph', () => {
-    cy.visit('/');
-    cy.contains('button','Add Panel').should('be.visible').click();
-    cy.get('.execute-btn').should('have.length',2); //check if there are 2 execute buttons, 1 for new panel
-  })
-  
-  
+it('allows adding a graph', () => {
+  cy.visit('/');
+  cy.contains('button', 'Add Panel').should('be.visible').click();
+  cy.get('.execute-btn').should('have.length', 2);
+})
+
+it('can switch from dark to light mode', () => {
+  cy.visit('/');
+  cy.get('[title="Use light theme"]').should('not.be.disabled').click();
+  cy.get('[class="bootstrap-dark"').should('not.exist');
+})
