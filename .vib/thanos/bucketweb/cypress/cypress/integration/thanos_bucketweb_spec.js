3202a4637bc677b10774ae53230e7e23b65f0d00
alukic <alukic@vmware.com>
2022-04-05 15:27:49 +0200

    Removed extra assertion
    Signed-off-by: alukic <alukic@vmware.com>

--- .vib/thanos/bucketweb/cypress/cypress/integration/thanos_bucketweb_spec.js
+++ .vib/thanos/bucketweb/cypress/cypress/integration/thanos_bucketweb_spec.js
@@ -4,7 +4,7 @@ it('allows to see runtime & build information', () => {
     cy.visit('/');
     cy.contains('.dropdown-toggle', 'Status').click();
     cy.contains('[class="dropdown-item"]', 'Runtime & Build Information').click();
-    cy.contains('Runtime Information').should('be.visible');
+    cy.contains('Runtime Information');
     cy.get('h2').contains('Build Information').should('be.visible');
 })
 
