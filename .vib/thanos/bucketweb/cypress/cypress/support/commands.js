37a6245756f54cbb0b4e4607a5991e9bb70e82e1
alukic <alukic@vmware.com>
2022-04-06 09:08:59 +0200

    Formatting
    Signed-off-by: alukic <alukic@vmware.com>

--- .vib/thanos/bucketweb/cypress/cypress/support/commands.js
+++ .vib/thanos/bucketweb/cypress/cypress/support/commands.js
@@ -1,13 +1,13 @@
-const COMMAND_DELAY = 1000;
+const COMMAND_DELAY = 1000
 
 for (const command of ['click']) {
-    Cypress.Commands.overwrite(command, (originalFn, ...args) => {
-        const origVal = originalFn(...args);
+  Cypress.Commands.overwrite(command, (originalFn, ...args) => {
+    const origVal = originalFn(...args)
 
-        return new Promise((resolve) => {
-            setTimeout(() => {
-                resolve(origVal);
-            }, COMMAND_DELAY);
-        });
-    });
+    return new Promise((resolve) => {
+      setTimeout(() => {
+        resolve(origVal)
+      }, COMMAND_DELAY)
+    })
+  })
 }
