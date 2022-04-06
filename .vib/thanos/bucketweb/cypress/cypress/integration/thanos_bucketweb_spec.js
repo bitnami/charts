eb12fbb596fa7b773f8e2c82b989f4dabcc37986
alukic <alukic@vmware.com>
2022-04-06 08:43:51 +0200

    Applied code review suggestions
    Signed-off-by: alukic <alukic@vmware.com>

--- .vib/thanos/bucketweb/cypress/cypress/integration/thanos_bucketweb_spec.js
+++ .vib/thanos/bucketweb/cypress/cypress/integration/thanos_bucketweb_spec.js
@@ -5,8 +5,8 @@ it('allows to see runtime & build information', () => {
     cy.contains('.dropdown-toggle', 'Status').click();
     cy.contains('[class="dropdown-item"]', 'Runtime & Build Information').click();
     cy.contains('Runtime Information');
-    cy.contains('h2','Build Information');
-    cy.contains('.capitalize-title','Version');
+    cy.contains('h2', 'Build Information');
+    cy.contains('.capitalize-title', 'Version');
 })
 
 it('allows to see the Blocks page', () => {
--- .vib/thanos/queryfrontend/cypress/cypress/integration/thanos_queryfrontend_spec.js
+++ .vib/thanos/queryfrontend/cypress/cypress/integration/thanos_queryfrontend_spec.js
@@ -6,14 +6,15 @@ it('allows to see runtime & build information', () => {
   cy.contains('[class="dropdown-item"]', 'Runtime & Build Information').click();
   cy.contains('Runtime Information');
   cy.contains('h2', 'Build Information');
-  cy.contains('.capitalize-title','Version');
+  cy.contains('.capitalize-title', 'version').siblings().should('not.be.empty'); //check if there is value for app and Go Version
+  cy.contains('.capitalize-title', 'goVersion').siblings().should('not.be.empty');
 })
 
 it('allows the execution of a query', () => {
   const QUERY_ALERT = 'No data queried yet';
   const QUERY_KEYWORD = 'vector';
   const QUERY_VALUE = '3000';
-  
+
   cy.visit('/graph');
   cy.contains('.alert', QUERY_ALERT);
   cy.get('.cm-line').type(QUERY_KEYWORD, '{enter}').type(`(${QUERY_VALUE})`).click();
@@ -26,7 +27,6 @@ it('allows listing all installed stores', () => {
   cy.contains('[class="nav-link"]', 'Stores').click();
   cy.get('[data-testid="endpoint"]').should('be.visible');
   cy.get('[data-testid="health"]').contains('UP');
-  cy.get('.badge-success').should('exist');
 })
 
 it('allows adding a graph', () => {
