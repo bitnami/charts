f6af01ee7877673ec9b1814f4e401c4b744dc7bd
alukic <alukic@vmware.com>
2022-04-07 08:50:45 +0200

    Added type delay to fix flakiness
    Signed-off-by: alukic <alukic@vmware.com>

--- .vib/thanos/queryfrontend/cypress/cypress/support/commands.js
+++ .vib/thanos/queryfrontend/cypress/cypress/support/commands.js
@@ -1,4 +1,5 @@
-const COMMAND_DELAY = 1300;
+const CLICK_DELAY = 1300;
+const TYPE_DELAY = 300;
 
 for (const command of ['click']) {
     Cypress.Commands.overwrite(command, (originalFn, ...args) => {
@@ -7,7 +8,19 @@ for (const command of ['click']) {
         return new Promise((resolve) => {
             setTimeout(() => {
                 resolve(origVal);
-            }, COMMAND_DELAY);
+            }, CLICK_DELAY);
+        });
+    });
+}
+
+for (const command of ['type']) {
+    Cypress.Commands.overwrite(command, (originalFn, ...args) => {
+        const origVal = originalFn(...args);
+
+        return new Promise((resolve) => {
+            setTimeout(() => {
+                resolve(origVal);
+            }, TYPE_DELAY);
         });
     });
 }
