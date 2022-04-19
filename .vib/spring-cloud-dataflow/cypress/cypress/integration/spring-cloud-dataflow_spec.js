dfc0fffeb7b28d1008165dfef0fa7c9480b64afc
superaleks <alukic@vmware.com>
2022-04-19 10:19:38 +0200

    Added additional tests for Spring Cloud Dataflow
    Signed-off-by: superaleks <alukic@vmware.com>

--- .vib/spring-cloud-dataflow/cypress/cypress/integration/spring-cloud-dataflow_spec.js
+++ .vib/spring-cloud-dataflow/cypress/cypress/integration/spring-cloud-dataflow_spec.js
@@ -1,7 +1,34 @@
 /// <reference types="cypress" />
 
-it('allows accesing the dashboard', () => {
+// it('allows accesing the dashboard', () => {
+//   cy.visit('/');
+//   cy.get('.signpost-trigger').click();
+//   cy.get('.signpost-content-body').should('contain', 'Version');
+// });
+
+// it('allows importing an application ', () => {
+//   cy.visit('/');
+//   cy.contains('button', 'Add application(s)').click();
+//   cy.contains(
+//     '.clr-control-label',
+//     'Stream application starters for Kafka/Maven'
+//   ).click();
+//   cy.get('.btn-primary').click();
+//   cy.get('.toast-container').should('contain', 'Application(s) Imported');
+//   cy.get('.content-area')
+//     .should('contain', 'processor')
+//     .and('contain', 'sink')
+//     .and('contain', 'source');
+// });
+
+it('allows creating a stream', () => {
   cy.visit('/');
-  cy.get('.signpost-trigger').click();
-  cy.get('.signpost-content-body').should('contain', 'Version');
+  cy.get('[routerlink="streams/list"').click();
+  cy.contains('.btn-primary', 'Create stream(s)').click();
+});
+
+it('allows creating a task', () => {
+  cy.visit('/');
+  cy.get('[routerlink="tasks-jobs/tasks"]').click();
+  cy.contains('.btn-primary', 'Create task').click();
 });
