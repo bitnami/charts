aca80bc44a2ded80860a8e8aa9800504d2396d00
alukic <alukic@vmware.com>
2022-04-21 14:48:42 +0200

    Added back the Goss tests
    Signed-off-by: alukic <alukic@vmware.com>

--- .vib/spring-cloud-dataflow/cypress/cypress/integration/utils.js
+++ .vib/spring-cloud-dataflow/cypress/cypress/integration/utils.js
@@ -1,5 +1,4 @@
 /// <reference types="cypress" />
-//Adding a comment to run VIB again
 export let random = (Math.random() + 1).toString(36).substring(7);
 
 export const importAStreamApplication = () => {
