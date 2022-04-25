a24901c2d93f81c29007b096b18d4e925b4330ce
alukic <alukic@vmware.com>
2022-04-25 17:05:06 +0200

    Removed files
    Signed-off-by: alukic <alukic@vmware.com>

--- .vib/keycloak/temp/enter_pod_shell.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/sh
-
-#ENTER POD SHELL
-kubectl exec --stdin --tty keycloak-0 -- /bin/bash
--- .vib/keycloak/temp/run_goss_tests.sh
+++
@@ -1,7 +0,0 @@
-puzzle run goss \
-  -p resources.url=file:///Users/alukic/Documents/Code/charts/.vib/keycloak/ \
-  -p config.remote.host=keycloak-0  \
-  -p config.remote.namespace=default \
-  -p config.remote.credentials=$(cat ~/.kube/config | base64)
-
-
