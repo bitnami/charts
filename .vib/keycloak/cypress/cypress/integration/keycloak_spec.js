afdf3c7c52482959743c5f3385a90048bbf6961b
alukic <alukic@vmware.com>
2022-04-28 10:40:22 +0200

    Changed the fixtures to plural
    Signed-off-by: alukic <alukic@vmware.com>

--- .vib/keycloak/cypress/cypress/integration/keycloak_spec.js
+++ .vib/keycloak/cypress/cypress/integration/keycloak_spec.js
@@ -8,7 +8,7 @@ it('allows login/logout and user manipulation', () => {
   cy.contains('a', 'Manage account').click({ force: true });
   cy.contains('p', 'Account console loading').should('not.exist'); //Wait until the loading is finished to continue with actions
   cy.contains('a', 'Personal').click({ force: true });
-  cy.fixture('user').then((user) => {
+  cy.fixture('users').then((user) => {
     cy.get('#first-name').clear().type(`${user.newUser.firstName}.${random}`);
     cy.get('#last-name').clear().type(`${user.newUser.lastName}.${random}`);
     cy.get('#email-address').clear().type(`${user.newUser.email}`);
@@ -23,7 +23,7 @@ it('allows creating a new user', () => {
   cy.login();
   cy.get('[data-ng-show="access.queryUsers"]').click();
   cy.get('#createUser').click();
-  cy.fixture('user').then((user) => {
+  cy.fixture('users').then((user) => {
     cy.get('#username').type(`${user.newUser.username}.${random}`);
     cy.get('#email').type(`${user.newUser.email}.${random}`);
     cy.get('#firstName').type(`${user.newUser.firstName}.${random}`);
@@ -37,7 +37,7 @@ it('allows the upload and delete of a client ', () => {
   cy.login();
   cy.contains('Import').click();
   cy.get('input#import-file').selectFile(
-    'cypress/fixtures/import-client.json',
+    'cypress/fixtures/import-clients.json',
     {
       force: true,
     }
@@ -57,7 +57,7 @@ it('allows adding and removing an identity provider', () => {
   cy.login();
   cy.contains('a', 'Identity Providers').click();
   cy.get('.form-group > .form-control').select(IDENTITY_PROVIDER);
-  cy.fixture('identity-provider').then((identity) => {
+  cy.fixture('identity-providers').then((identity) => {
     cy.get('#clientId').type(`${identity.identityProvider.clientId}.${random}`);
     cy.get('#clientSecret').type(
       `${identity.identityProvider.clientSecret}.${random}`
@@ -73,7 +73,7 @@ it('allows adding and removing an identity provider', () => {
 it('allows creating a SMTP host', () => {
   cy.login();
   cy.contains('a', 'Email').click();
-  cy.fixture('smtp').then((smtp) => {
+  cy.fixture('smtps').then((smtp) => {
     cy.get('#smtpHost').clear().type(`${smtp.smtp.host}.${random}`);
     cy.get('#smtpFrom').clear().type(`${smtp.smtp.from}`);
   });
