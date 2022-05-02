9f034b6f9c39db51834264bd59c58f7efc426599
alukic <alukic@vmware.com>
2022-05-02 10:15:02 +0200

    Applying PR suggestions
    Signed-off-by: alukic <alukic@vmware.com>

--- .vib/spring-cloud-dataflow/cypress/cypress/integration/prepare-app-state.js
+++ .vib/spring-cloud-dataflow/cypress/cypress/integration/prepare-app-state.js
@@ -9,11 +9,11 @@ export const importAnApplication = (application) => {
   cy.get('.toast-container').should('contain', 'Application(s) Imported');
 };
 
-export const createATask = (task) => {
+export const createATask = (taskType) => {
   cy.visit('/dashboard/#/tasks-jobs/tasks');
   cy.contains('button', 'Create task').click();
-  cy.get('.CodeMirror-line').type(task);
-  cy.contains('#v-2', task);
+  cy.get('.CodeMirror-line').type(taskType);
+  cy.contains('#v-2', taskType);
   cy.contains('button', 'Create task').click();
   cy.contains('.modal-content', 'Create task');
   cy.fixture('tasks').then((task) => {
