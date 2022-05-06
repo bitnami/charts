c13162d7b5e939c67c95634a198904b43328e866
alukic <alukic@vmware.com>
2022-05-06 15:40:28 +0200

    Added improvements to Kibana tests
    Signed-off-by: alukic <alukic@vmware.com>

---
+++ .vib/kibana/cypress/cypress/integration/prepare-app-state.js
@@ -0,0 +1,18 @@
+/// <reference types="cypress" />
+
+export const skipTheWelcomeScreen = () => {
+  cy.visit('/');
+  closeThePopups();
+  cy.get('body').then(($body) => {
+    if ($body.text().includes('Explore on my own')) {
+      cy.get('button[data-test-subj="skipWelcomeScreen"]').click();
+    }
+  });
+};
+
+export const closeThePopups = () => {
+  cy.get('[data-test-subj="kbnLoadingMessage"]').should('not.exist');
+  cy.get('[data-test-subj="toastCloseButton"]').click({
+    multiple: true,
+  });
+};
