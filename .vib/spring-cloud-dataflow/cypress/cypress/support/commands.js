8d67e1d058e1be88b768ad9230e97da8dd7b23cd
superaleks <47592680+superaleks@users.noreply.github.com>
2022-05-11 17:17:08 +0200

    [bitnami/spring-cloud-dataflow] Added the Spring Cloud Dataflow Helm chart tests (#9835)
    * Created the Spring Cloud Dataflow tests
    
    Signed-off-by: alukic <alukic@vmware.com>
    
    * Trigger VIB for Spring Cloud Dataflow
    
    Signed-off-by: alukic <alukic@vmware.com>
    
    * Trigger version bump too
    
    Signed-off-by: alukic <alukic@vmware.com>
    
    * Removed comments
    
    Signed-off-by: alukic <alukic@vmware.com>
    
    * Change the pipeline syntax
    
    Signed-off-by: alukic <alukic@vmware.com>
    
    * Improved the tests, getting ready for the review'
    
    Signed-off-by: alukic <alukic@vmware.com>
    
    * Improved the tests
    
    Signed-off-by: alukic <alukic@vmware.com>
    
    * Added back the Goss tests
    
    Signed-off-by: alukic <alukic@vmware.com>
    
    * Made Goss tests even more minimalistic
    
    Signed-off-by: alukic <alukic@vmware.com>
    
    * Remove the test modification of files
    
    Signed-off-by: alukic <alukic@vmware.com>
    
    * Passed trough linter
    
    Signed-off-by: alukic <alukic@vmware.com>
    
    * Applied PR design suggestion
    
    Signed-off-by: alukic <alukic@vmware.com>
    
    * Updated tests, fixing the flaw in the design
    
    Signed-off-by: alukic <alukic@vmware.com>
    
    * Made changes in test design
    
    Signed-off-by: alukic <alukic@vmware.com>
    
    * Removed the plugin
    
    Signed-off-by: alukic <alukic@vmware.com>
    
    * Removed extra spaces
    
    Signed-off-by: alukic <alukic@vmware.com>
    
    * Fix the formatting issues
    
    Signed-off-by: alukic <alukic@vmware.com>
    
    * Remove a console log
    
    Signed-off-by: alukic <alukic@vmware.com>
    
    * Changed the locator from CSS selector
    
    Signed-off-by: alukic <alukic@vmware.com>
    
    * Applied the CR suggestions
    
    Signed-off-by: alukic <alukic@vmware.com>
    
    * Added regex to check if there are numbers in version
    
    Signed-off-by: alukic <alukic@vmware.com>
    
    * Improved code slightly
    
    Signed-off-by: alukic <alukic@vmware.com>
    
    * Apply PR suggestions
    
    Signed-off-by: alukic <alukic@vmware.com>
    
    * Applying PR suggestions
    
    Signed-off-by: alukic <alukic@vmware.com>
    
    * Applying PR suggestions
    
    Signed-off-by: alukic <alukic@vmware.com>
    
    * Locator change from CSS to text
    
    Signed-off-by: alukic <alukic@vmware.com>
    
    * Formatting, locator change
    
    Signed-off-by: alukic <alukic@vmware.com>
    
    * Used fixture instead of static value
    
    Signed-off-by: alukic <alukic@vmware.com>
    
    * Replaced CSS selectors with text
    
    Signed-off-by: alukic <alukic@vmware.com>
    
    * Apply review suggestions
    
    Signed-off-by: alukic <alukic@vmware.com>
    
    * Reorganised the fixtures
    
    Signed-off-by: alukic <alukic@vmware.com>
    
    * Removed the Plugins
    
    Signed-off-by: alukic <alukic@vmware.com>
    
    * Fix a typo
    
    Signed-off-by: alukic <alukic@vmware.com>
    
    * Non-o instead od 1001 user
    
    Signed-off-by: alukic <alukic@vmware.com>
    
    * Changed the Goss test
    
    Signed-off-by: alukic <alukic@vmware.com>
    
    * User ID test improvement, fixture fix
    
    Signed-off-by: alukic <alukic@vmware.com>
    
    * Renamed files
    
    Signed-off-by: alukic <alukic@vmware.com>
    
    * Applied peer review comments
    
    Signed-off-by: alukic <alukic@vmware.com>

---
+++ .vib/spring-cloud-dataflow/cypress/cypress/support/commands.js
@@ -0,0 +1,26 @@
+const CLICK_DELAY = 800;
+const TYPE_DELAY = 200;
+
+for (const command of ['click']) {
+  Cypress.Commands.overwrite(command, (originalFn, ...args) => {
+    const origVal = originalFn(...args);
+
+    return new Promise((resolve) => {
+      setTimeout(() => {
+        resolve(origVal);
+      }, CLICK_DELAY);
+    });
+  });
+}
+
+for (const command of ['type']) {
+  Cypress.Commands.overwrite(command, (originalFn, ...args) => {
+    const origVal = originalFn(...args);
+
+    return new Promise((resolve) => {
+      setTimeout(() => {
+        resolve(origVal);
+      }, TYPE_DELAY);
+    });
+  });
+}
