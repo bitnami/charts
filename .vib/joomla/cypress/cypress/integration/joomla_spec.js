12e13add9b47dd9101b0e0eb6903184cc2f73096
FraPazGal <fdepaz@vmware.com>
2022-06-21 16:55:35 +0200

    Redo API test
    Signed-off-by: FraPazGal <fdepaz@vmware.com>

--- .vib/joomla/cypress/cypress/integration/joomla_spec.js
+++ .vib/joomla/cypress/cypress/integration/joomla_spec.js
@@ -14,9 +14,11 @@ it('allows creating an article', () => {
   cy.visit('/administrator/index.php?option=com_content&view=articles');
   cy.get('.button-new').click();
   cy.fixture('articles').then((article) => {
-    cy.get('#jform_title').type(`${article.newArticle.title} ${random}`, {
-      force: true,
-    });
+    cy.get('#jform_title')
+      .scrollIntoView()
+      .type(`${article.newArticle.title} ${random}`, {
+        force: true,
+      });
   });
   cy.contains('Save & Close').click();
   cy.contains('Article saved');
@@ -38,6 +40,7 @@ it('allows modifying site settings', () => {
   cy.visit('/administrator/index.php?option=com_config');
   cy.fixture('site-settings').then((siteSettings) => {
     cy.get('#jform_sitename')
+      .scrollIntoView()
       .clear({ force: true })
       .type(`${siteSettings.newSiteSetting.sitename} ${random}`);
   });
@@ -46,25 +49,24 @@ it('allows modifying site settings', () => {
 });
 
 it('checks plugin management and API operabilty', () => {
-  // Before using basic auth for the API, we first must enable via plugin
-  // Ref: https://docs.joomla.org/J4.x:Joomla_Core_APIs
   cy.login();
-  cy.visit('/administrator/index.php?option=com_plugins&view=plugins');
-  cy.get("[title*='Basic Auth']").click({ force: true });
-  cy.get('#jform_enabled').select('1', { force: true });
-  cy.contains('Save & Close').click();
-  cy.contains('Plugin saved');
-
-  cy.request({
-    method: 'GET',
-    url: '/api/index.php/v1/banners/categories',
-    form: true,
-    auth: {
-      username: Cypress.env('username'),
-      password: Cypress.env('password'),
-    },
-  }).then((response) => {
-    expect(response.status).to.eq(200);
-    expect(response.body).to.include.all.keys('links', 'data');
-  });
+  cy.contains('User Menu').click();
+  cy.contains('Edit Account').click();
+  cy.contains('API Token').click({ force: true });
+  cy.get('#jform_joomlatoken_token')
+    .invoke('attr', 'value')
+    .as('APIToken')
+    .then((APIToken) => {
+      cy.request({
+        method: 'GET',
+        url: '/api/index.php/v1/banners/categories',
+        form: true,
+        auth: {
+          bearer: APIToken,
+        },
+      }).then((response) => {
+        expect(response.status).to.eq(200);
+        expect(response.body).to.include.all.keys('links', 'data');
+      });
+    });
 });
