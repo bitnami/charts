4f74e7f920834b4e7241abb7f2c5826a856945ef
alukic <alukic@vmware.com>
2022-06-22 12:20:12 +0200

    Applied CR suggestions
    Signed-off-by: alukic <alukic@vmware.com>

--- .vib/consul/cypress/cypress/integration/consul_spec.js
+++ .vib/consul/cypress/cypress/integration/consul_spec.js
@@ -2,13 +2,16 @@
 
 import { random } from './utils';
 
+const DATACENTER_NAME = Cypress.env('datacenterName');
+const REPLICA_COUNT = Cypress.env('replicaCount');
+
 it('shows all instances of services', () => {
-  cy.visit('/ui/datacenter1/services');
+  cy.visit(`/ui/${DATACENTER_NAME}/services`);
   cy.contains('Services');
   cy.get('.linkable').contains('consul');
-  cy.get('.detail span').contains('3 instances');
+  cy.get('.detail span').contains(`${REPLICA_COUNT} instances`);
   cy.get('.linkable').click();
-  cy.get('[class="address"]').should('have.length', 3);
+  cy.get('[class="address"]').should('have.length', REPLICA_COUNT);
   cy.get('.ember-view').within(() => {
     cy.contains('.header', 'consul').click();
   });
@@ -16,26 +19,26 @@ it('shows all instances of services', () => {
 });
 
 it('shows the cluster overview ', () => {
-  cy.visit('/ui/datacenter1/overview/server-status');
+  cy.visit(`/ui/${DATACENTER_NAME}/overview/server-status`);
   cy.get('.consul-server-list').within(() => {
-    cy.get('li').should('have.length', 3);
+    cy.get('li').should('have.length', REPLICA_COUNT);
   });
   cy.get('.voting-status-leader').click();
   cy.contains('Agent alive and reachable');
 });
 
 it('allows the creation of key-value pairs', () => {
-  cy.visit('/ui/datacenter1/kv');
+  cy.visit(`/ui/${DATACENTER_NAME}/kv`);
   cy.contains('Create').click();
   cy.get('fieldset').within(() => {
-    cy.fixture('key').then((key) => {
+    cy.fixture('keys').then((key) => {
       cy.get('input').first().type(`${key.newKey.testKey}.${random}`);
     });
   });
   cy.get('[type="submit"]').click({ force: true });
   cy.get('.notice').contains('Success');
   cy.get('.tabular-collection').within(() => {
-    cy.fixture('key').then((key) => {
+    cy.fixture('keys').then((key) => {
       cy.contains(`${key.newKey.testKey}.${random}`);
     });
   });
@@ -48,9 +51,9 @@ it('shows configured data center', () => {
 });
 
 it('is able to display all of the nodes', () => {
-  cy.visit('/ui/datacenter1/nodes');
+  cy.visit(`/ui/${DATACENTER_NAME}/nodes`);
   cy.get('.ember-view').within(() => {
-    cy.get('.passing').should('have.length', 3);
+    cy.get('.passing').should('have.length', REPLICA_COUNT);
     cy.get('.passing').first().click();
   });
   cy.contains('Agent alive and reachable');
