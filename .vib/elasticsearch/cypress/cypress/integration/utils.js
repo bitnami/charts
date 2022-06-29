ff3f9a1f4083f27c36a3d99214b54ba173bc2dd3
alukic <alukic@vmware.com>
2022-06-29 10:26:43 +0200

    Increased platform size, improved code
    Signed-off-by: alukic <alukic@vmware.com>

--- .vib/elasticsearch/cypress/cypress/integration/utils.js
+++ .vib/elasticsearch/cypress/cypress/integration/utils.js
@@ -1,5 +1,7 @@
 /// <reference types="cypress" />
+
 export let random = (Math.random() + 1).toString(36).substring(7);
+
 export let randomNumber = (Math.floor(Math.random() * 10000) + 10000)
   .toString()
   .substring(1);
