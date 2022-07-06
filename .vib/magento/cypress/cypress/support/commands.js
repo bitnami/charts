7974a9ae7f80b28094cee856a801e44847fa0e0c
alukic <alukic@vmware.com>
2022-07-06 15:23:21 +0200

    Added publishing change
    Signed-off-by: alukic <alukic@vmware.com>

--- .vib/magento/cypress/cypress/support/commands.js
+++ .vib/magento/cypress/cypress/support/commands.js
@@ -1,5 +1,5 @@
-const CLICK_DELAY = 1000;
-const GET_DELAY = 1000;
+const CLICK_DELAY = 0;
+const GET_DELAY = 500;
 const BASE_URL = 'http://vmware-magento.my';
 
 for (const command of ['click']) {
