10c633f25b845847f8e5ce8880552b24543b0733
alukic <alukic@vmware.com>
2022-07-06 10:24:02 +0200

    Improved the last Cypress test
    Signed-off-by: alukic <alukic@vmware.com>

--- .vib/magento/cypress/cypress/support/commands.js
+++ .vib/magento/cypress/cypress/support/commands.js
@@ -47,7 +47,7 @@ Cypress.Commands.add(
 );
 
 Cypress.on('uncaught:exception', (err, runnable) => {
-  // we expect a 3rd party library error with message 'list not defined'
+  // we expect an application error with message 'rendering locks'
   // and don't want to fail the test so we return false
   if (err.message.includes('renderingLocks')) {
     return false;
