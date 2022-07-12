a0f9aaa03c7771fa0528f025e992b48b8e5fa8fb
alukic <alukic@vmware.com>
2022-07-12 13:28:56 +0200

    Added Goss to the publishing pipeline as well
    Signed-off-by: alukic <alukic@vmware.com>

--- .vib/odoo/cypress/cypress/integration/utils.js
+++ .vib/odoo/cypress/cypress/integration/utils.js
@@ -1,20 +1,3 @@
 /// <reference types="cypress" />
 
 export let random = (Math.random() + 1).toString(36).substring(7);
-
-export let installSalesModule = () => {
-  //   cy.login();
-  //   cy.contains('Sales').click();
-  //   cy.get('[title="Sales"]').within(() => {
-  //     cy.contains('Install').click();
-  //   });
-};
-
-export let uninstallSalesModule = () => {
-  cy.login();
-  cy.get('[title="Home Menu"]').click();
-  cy.contains('Apps').click();
-  cy.get('[title="Sales"]').within(() => {
-    cy.get('[title="Dropdown menu"]').click();
-  });
-};
