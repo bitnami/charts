7ca353e8b4297aad4063301ac471b1daac8adab1
Miguel Ruiz <miruiz@vmware.com>
2022-08-04 16:16:43 +0200

    [bitnami/schema-registry] Add Goss/Cypress tests
    Signed-off-by: Miguel Ruiz <miruiz@vmware.com>

---
+++ .vib/schema-registry/cypress/cypress/integration/schema_registry_spec.js
@@ -0,0 +1,35 @@
+/// <reference types="cypress" />
+import body from '../fixtures/schema.json';
+
+it('can access the API and obtain global compatibility level', () => {
+  cy.request({
+    method: 'GET',
+    url: '/config',
+    form: true,
+  }).then((response) => {
+    expect(response.status).to.eq(200);
+    expect(response.body.compatibilityLevel).to.contain('BACKWARD');
+  });
+});
+
+it('can access the API and create a new schema in the "test" subject', () => {
+  cy.request({
+    method: 'POST',
+    headers: { 'Content-Type': 'application/vnd.schemaregistry.v1+json' },
+    url: 'subjects/test/versions',
+    body: body,
+  }).then((response) => {
+    expect(response.status).to.eq(200);
+    expect(response.body.id).to.eq(1);
+  });
+
+  cy.request({
+    method: 'GET',
+    url: 'subjects',
+    form: true,
+  }).then((response) => {
+    expect(response.status).to.eq(200);
+    expect(response.body).to.have.length(1);
+    expect(response.body).to.include('test');
+  });
+});
