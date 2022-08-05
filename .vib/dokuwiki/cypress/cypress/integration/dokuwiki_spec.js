08154b9fefa65c93161a9b4093abd23083294ad4
FraPazGal <fdepaz@vmware.com>
2022-08-05 15:47:50 +0200

    Remove redundant test
    Signed-off-by: FraPazGal <fdepaz@vmware.com>

--- .vib/dokuwiki/cypress/cypress/integration/dokuwiki_spec.js
+++ .vib/dokuwiki/cypress/cypress/integration/dokuwiki_spec.js
@@ -2,6 +2,7 @@
 import { random } from '../support/utils';
 
 it('allows editing the starting page', () => {
+  cy.login();
   cy.visit('/start');
   cy.get('a[href="/start?do=edit"]').click({ force: true });
   cy.fixture('pages').then((page) => {
@@ -10,18 +11,4 @@ it('allows editing the starting page', () => {
     cy.reload();
     cy.contains(`${page.newPage.content}.${random}`);
   });
-});
-
-it('allows registering users', () => {
-  cy.login();
-  cy.visit('login?do=admin&page=usermanager');
-  cy.fixture('users').then((user) => {
-    cy.get('#add_userid').type(`${user.newUser.userName}.${random}`);
-    cy.get('#add_userpass').type(`${user.newUser.password}.${random}`);
-    cy.get('#add_userpass2').type(`${user.newUser.password}.${random}`);
-    cy.get('#add_username').type(`${user.newUser.realName}.${random}`);
-    cy.get('#add_usermail').type(`${user.newUser.email}.${random}`);
-  });
-  cy.contains('button', 'Add').click();
-  cy.contains('User added successfully');
 });
