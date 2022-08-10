45ea5014bfba82e57f04f98363d2daa85c31703d
Jose Antonio Carmona <jcarmona@vmware.com>
2022-08-10 12:46:26 +0200

    [bitnami/phpbb] Add test and publish using VIB
    Signed-off-by: Jose Antonio Carmona <jcarmona@vmware.com>

---
+++ .vib/phpbb/cypress/cypress/support/commands.js
@@ -0,0 +1,23 @@
+const COMMAND_DELAY = 2000;
+
+for (const command of ['click']) {
+  Cypress.Commands.overwrite(command, (originalFn, ...args) => {
+    const origVal = originalFn(...args);
+
+    return new Promise((resolve) => {
+      setTimeout(() => {
+        resolve(origVal);
+      }, COMMAND_DELAY);
+    });
+  });
+}
+
+Cypress.Commands.add(
+  'login',
+  (username = Cypress.env('username'), password = Cypress.env('password')) => {
+    cy.visit('/ucp.php?mode=login&redirect=index.php');
+    cy.get('#username').type(username);
+    cy.get('#password').type(password);
+    cy.get('[name=login]').click();
+  }
+);
