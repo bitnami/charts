23cec1f567af9883071d310024abe858cbcaa99e
Jose Antonio Carmona <jcarmona@vmware.com>
2022-08-22 16:50:31 +0200

    Apply feedback
    Signed-off-by: Jose Antonio Carmona <jcarmona@vmware.com>

--- .vib/tomcat/cypress/cypress/integration/tomcat_spec.js
+++ .vib/tomcat/cypress/cypress/integration/tomcat_spec.js
@@ -3,7 +3,6 @@ import { random } from '../support/utils';
 
 it('allows to deploy a new WAR app', () => {
   cy.visitAuth(`/manager`);
-  cy.contains('Web Application Manager');
   cy.contains('form', 'Select WAR file').within(() => {
     cy.get('[type="file"]').selectFile({
       contents: 'cypress/fixtures/sample.war',
