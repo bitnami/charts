8d758e52781c517abd08ac46dfdff5414ecefa40
Michiel <michield@vmware.com>
2022-08-23 15:45:04 +0200

    Changes to tests based on comments
    Signed-off-by: Michiel <michield@vmware.com>

--- .vib/moodle/cypress/cypress/integration/moodle_spec.js
+++ .vib/moodle/cypress/cypress/integration/moodle_spec.js
@@ -1,18 +1,17 @@
 /// <reference types="cypress" />
+import { random } from '../support/utils';
 
-it('adds new course', () => {
+it('allows adding a new course with image', () => {
   cy.login();
-  cy.contains('a', 'Site administration').click();
-  cy.contains('a', 'Courses').click();
-  cy.contains('a', 'Add a new course').click();
+  cy.visit('course/edit.php');
   cy.fixture('courses').then((courses) => {
-    cy.get('#id_fullname').type(courses.newCourse.fullName);
-    cy.get('#id_shortname').type(courses.newCourse.shortName);
+    cy.get('#id_fullname').type(`${courses.newCourse.fullName}.${random}`);
+    cy.get('#id_shortname').type(`${courses.newCourse.shortName}.${random}`);
   });
   cy.get('.fp-btn-add').click();
   cy.contains('a', 'Upload a file').click();
   cy.get('input[type="file"]').selectFile(
-    'cypress/fixtures/images/post_image.png',
+    'cypress/fixtures/images/test_image.jpeg',
     {
       force: true,
     }
@@ -21,6 +20,6 @@ it('adds new course', () => {
   cy.contains('input', 'Save and display').click();
   cy.visit('/my/courses.php');
   cy.fixture('courses').then((courses) => {
-    cy.contains(courses.newCourse.fullName);
+    cy.contains(`${courses.newCourse.fullName}.${random}`);
   });
 });
