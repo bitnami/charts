dfdca46015c2e7531263249de7409d9ebae41009
Michiel <michield@vmware.com>
2022-08-31 10:59:05 +0200

    Remove extra image check
    Signed-off-by: Michiel <michield@vmware.com>

--- .vib/drupal/cypress/cypress/integration/drupal_spec.js
+++ .vib/drupal/cypress/cypress/integration/drupal_spec.js
@@ -7,7 +7,6 @@ it('allows creating an article with an image', () => {
   cy.fixture('articles').then((article) => {
     cy.get('.text-full[name*=title]').type(`${article.newArticle.title} ${random}`);
     cy.get('[type=file]').get('[type=file]').selectFile('cypress/fixtures/images/test_image.jpeg', { force: true });
-    cy.get('[src*="test_image"]');
     cy.get('input[id*="alt"]').type(`${article.newArticle.altText}`);
     cy.contains('Save').click();
     cy.visit('/admin/content');
