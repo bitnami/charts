16d398e61ae4ba131b118e3afdb2c769ffa46789
Jose Antonio Carmona <jcarmona@vmware.com>
2022-08-31 18:53:38 +0200

    Update comments in Cypress tests
    Signed-off-by: Jose Antonio Carmona <jcarmona@vmware.com>

--- .vib/haproxy-intel/cypress/cypress/integration/haproxy-intel_spec.js
+++ .vib/haproxy-intel/cypress/cypress/integration/haproxy-intel_spec.js
@@ -1,8 +1,8 @@
 /// <reference types="cypress" />
 
 it('can access backend server', () => {
-  // HAProxy is configured with an NGINX server deployed as a sidecar, which
-  // plays the role of backend. Successful access to NGINX means HAProxy is
+  // HAProxy-Intel is configured with an NGINX server deployed as a sidecar, which
+  // plays the role of backend. Successful access to NGINX means HAProxy-Intel is
   // correctly routing!
   cy.visit('/');
   cy.contains('Welcome to nginx')
