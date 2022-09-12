0e3ad658fdb88655c463acddbb7cecb492ef0153
Gonzalo Gomez Gracia <gonzalog@vmware.com>
2022-09-12 13:36:45 +0200

    [bitnami/keycloak] Update selectors
    Signed-off-by: Gonzalo Gomez Gracia <gonzalog@vmware.com>

--- .vib/keycloak/cypress/cypress/integration/keycloak_spec.js
+++ .vib/keycloak/cypress/cypress/integration/keycloak_spec.js
@@ -4,21 +4,21 @@ import { random } from '../support/utils';
 it('allows creating a new user', () => {
   cy.login();
   cy.get('[href*="/users"]').click();
-  cy.get('button[data-testid="add-user"]').click();
+  cy.get('[data-testid="add-user"]').click();
   cy.fixture('users').then((user) => {
     cy.get('#kc-username').type(`${user.newUser.username}.${random}`);
-    cy.get('input[data-testid="firstName-input"]').type(`${user.newUser.firstName}.${random}`);
-    cy.get('input[data-testid="lastName-input"]').type(`${user.newUser.lastName}.${random}`);
+    cy.get('[data-testid="firstName-input"]').type(`${user.newUser.firstName}.${random}`);
+    cy.get('[data-testid="lastName-input"]').type(`${user.newUser.lastName}.${random}`);
   });
-  cy.get('button[data-testid="create-user"]').click();
-  cy.contains('h4', 'The user has been created');
+  cy.get('[data-testid="create-user"]').click();
+  cy.contains('The user has been created');
 });
 
 it('import and check user information', () => {
   cy.login();
   cy.get('[href*="/realm-settings"]').click();
-  cy.get('div[data-testid="action-dropdown"]').click();
-  cy.get('a[data-testid="openPartialImportModal"]').click();
+  cy.get('[data-testid="action-dropdown"]').click();
+  cy.get('[data-testid="openPartialImportModal"]').click();
   const importFile = 'cypress/fixtures/import-data.json';
   const randomUsername = `test-realm-user-${random}`;
   const randomEmail = `test-realm-user-${random}@example.com`;
@@ -30,10 +30,10 @@ it('import and check user information', () => {
     const jsonText = JSON.stringify(obj);
     cy.get('.view-line').type(jsonText, { parseSpecialCharSequences: false, delay: 1 });
   });
-  cy.get('input[data-testid="users-checkbox"]').click();
-  cy.get('button[data-testid="import-button"]').click();
-  cy.contains('h4', 'One record added');
-  cy.get('button[data-testid="close-button"]').click();
+  cy.get('[data-testid="users-checkbox"]').click();
+  cy.get('[data-testid="import-button"]').click();
+  cy.contains('One record added');
+  cy.get('[data-testid="close-button"]').click();
   cy.get('[href*="/users"]').click();
-  cy.contains('td', randomEmail);
+  cy.contains(randomEmail);
 });
