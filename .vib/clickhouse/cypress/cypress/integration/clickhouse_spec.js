aa8e4cff22fddb0abf1ea2f2ea5c3eea57b8ec75
Javier Salmeron Garcia <jsalmeron@vmware.com>
2022-09-22 13:47:11 +0200

    [bitnami/clickhouse] test: :white_check_mark: Fix incorrect GOSS parameter --username
    Signed-off-by: Javier Salmeron Garcia <jsalmeron@vmware.com>

--- .vib/clickhouse/cypress/cypress/integration/clickhouse_spec.js
+++ .vib/clickhouse/cypress/cypress/integration/clickhouse_spec.js
@@ -2,8 +2,9 @@
 
 it('allows running a query', () => {
   cy.visit('/play');
-  cy.get('#user').clear();
-  cy.get('#user').type(Cypress.env('username'));
+  cy.get('#user')
+    .clear()
+    .type(Cypress.env('username'), { force: true });
   cy.get('#password').type(Cypress.env('password'));
   cy.fixture('queries').then((queries) => {
     cy.get('#query').type(queries.dbQuery.query);
