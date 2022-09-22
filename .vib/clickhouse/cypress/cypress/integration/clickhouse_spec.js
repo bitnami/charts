78d93c9138cdbfdfea36b02598f405ca23d7804f
Javier Salmeron Garcia <jsalmeron@vmware.com>
2022-09-22 12:50:32 +0200

    [bitnami/clickhouse] test: :white_check_mark: Adapt to testing best practices
    Signed-off-by: Javier Salmeron Garcia <jsalmeron@vmware.com>

--- .vib/clickhouse/cypress/cypress/integration/clickhouse_spec.js
+++ .vib/clickhouse/cypress/cypress/integration/clickhouse_spec.js
@@ -1,14 +1,16 @@
 /// <reference types="cypress" />
 
 it('allows running a query', () => {
-  cy.visit('/play')
-  cy.get('input[type="password"]').type(Cypress.env('password'));
-  cy.fixture('query').then((query) => {
-    cy.get('#query').type(query.query);
+  cy.visit('/play');
+  cy.get('#user').clear();
+  cy.get('#user').type(Cypress.env('username'));
+  cy.get('#password').type(Cypress.env('password'));
+  cy.fixture('queries').then((queries) => {
+    cy.get('#query').type(queries.dbQuery.query);
   });
   cy.get('#run').click();
-  cy.fixture('query').then((query) => {
-    cy.contains(query.output.header);
-    cy.contains(query.output.return);
+  cy.fixture('queries').then((queries) => {
+    cy.contains(queries.dbQuery.output.header);
+    cy.contains(queries.dbQuery.output.return);
   });
 });
