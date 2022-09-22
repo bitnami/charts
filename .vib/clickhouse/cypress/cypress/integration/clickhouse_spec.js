ea33669e66a7873afab3a933a448404749700be2
Javier Salmeron Garcia <jsalmeron@vmware.com>
2022-09-22 13:05:13 +0200

    [bitnami/clickhouse] test: :white_check_mark: Add runtime parameters to vib-publish and randomize goss database inputs
    Signed-off-by: Javier Salmeron Garcia <jsalmeron@vmware.com>

--- .vib/clickhouse/cypress/cypress/integration/clickhouse_spec.js
+++ .vib/clickhouse/cypress/cypress/integration/clickhouse_spec.js
@@ -7,9 +7,7 @@ it('allows running a query', () => {
   cy.get('#password').type(Cypress.env('password'));
   cy.fixture('queries').then((queries) => {
     cy.get('#query').type(queries.dbQuery.query);
-  });
-  cy.get('#run').click();
-  cy.fixture('queries').then((queries) => {
+    cy.get('#run').click();
     cy.contains(queries.dbQuery.output.header);
     cy.contains(queries.dbQuery.output.return);
   });
