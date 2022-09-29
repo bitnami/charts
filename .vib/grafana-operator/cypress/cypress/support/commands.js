4c446320283fa42372b494cdb8f4c51140a0a435
FraPazGal <fdepaz@vmware.com>
2022-09-29 16:49:59 +0200

    Remove unncessary exception handling
    Signed-off-by: FraPazGal <fdepaz@vmware.com>

--- .vib/grafana-operator/cypress/cypress/support/commands.js
+++ .vib/grafana-operator/cypress/cypress/support/commands.js
@@ -23,13 +23,3 @@ Cypress.Commands.add(
     cy.contains('Home');
   }
 );
-
-Cypress.on('uncaught:exception', (err, runnable) => {
-  // we expect a 3rd party library error with message 'list not defined'
-  // and don't want to fail the test so we return false
-  if (err.message.includes('unhandled promise rejection')) {
-    return false;
-  }
-  // we still want to ensure there are no other unexpected
-  // errors, so we let them fail the test
-});
