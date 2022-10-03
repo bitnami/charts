c458999e4348a3d9488342019a15a069b0444463
Michiel <michield@vmware.com>
2022-10-03 17:32:28 +0200

    Add download test for Cypress
    Signed-off-by: Michiel <michield@vmware.com>

--- .vib/owncloud/cypress/cypress/integration/owncloud_spec.js
+++ .vib/owncloud/cypress/cypress/integration/owncloud_spec.js
@@ -9,13 +9,26 @@ it('allows creating a folder and uploading a file ', () => {
     cy.get('[value="New folder"]').type(`${folder.newFolder.name}.${random}`);
     cy.contains('Create').click();
     cy.contains(`${folder.newFolder.name}.${random}`).click();
+    cy.get('span').contains(`${folder.newFolder.name}.${random}`).click();
+    cy.contains('No files in here');
+    cy.get('.new').click();
+    cy.get('[data-action="upload"]').click();
+    cy.get('input[type="file"]').selectFile(
+      'cypress/fixtures/file_to_upload.json',
+      { force: true }
+    );
+    cy.contains('file_to_upload').should('be.visible');
+    cy.get('[href*="file_to_upload"]')
+      .invoke('attr', 'href')
+      .then((href) => {
+        cy.log(href);
+        cy.request({
+          url: href,
+          method: 'GET',
+        }).then((response) => {
+            expect(response.status).to.eq(200);
+            expect(response.headers['content-type']).to.eq('text/plain;charset=UTF-8');
+          });
+      });
   });
-  cy.get('.new').click();
-  cy.get('[data-action="upload"]').click();
-  cy.get('input[type="file"]').selectFile(
-    'cypress/fixtures/file_to_upload.json',
-    { force: true }
-  );
-  cy.contains('file_to_upload').should('be.visible');
 });
-
