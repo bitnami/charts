ada2fd9493b123e38325daec7110a3bcc36184d1
Michiel <michield@vmware.com>
2022-10-11 16:35:40 +0200

    Simplify selectors
    Signed-off-by: Michiel <michield@vmware.com>

--- .vib/phpmyadmin/cypress/cypress/integration/phpmyadmin_spec.js
+++ .vib/phpmyadmin/cypress/cypress/integration/phpmyadmin_spec.js
@@ -8,22 +8,15 @@ it('allows creating a database and a table', () => {
     cy.get('#text_create_db').type(`${td.databaseName}.${random}`, {
       force: true,
     });
-    cy.get('#buttonGo').click({ force: true });
-    cy.get('.lock-page [type="text"]').type(`${td.tableName}.${random}`);
-    cy.get('.lock-page [type="number"]').clear().type(td.columnNumber);
-    cy.contains('[type="submit"]', 'Create').click();
+    cy.get('input').contains('Create').click({ force: true });
+    cy.get('[name="table"]').type(`${td.tableName}.${random}`);
+    cy.get('[name="num_fields"]').clear().type(td.columnNumber);
+    cy.get('[type="submit"]').click();
     cy.get('#field_0_1').type(`${td.columnName}.${random}`);
     cy.get('.btn-primary').click();
     cy.visit('index.php');
-    cy.contains('a', `${td.databaseName}.${random}`).scrollIntoView().click({
-      force: true,
-    });
-    cy.contains(
-      '#pma_navigation_tree_content',
-      `${td.tableName}.${random}`
-    ).click({
-      force: true,
-    });
+    cy.contains(`${td.databaseName}.${random}`).scrollIntoView().click({force: true});
+    cy.contains(`${td.tableName}.${random}`).click({force: true});
     cy.contains('.table-responsive-md', `${td.columnName}.${random}`);
   });
 });
