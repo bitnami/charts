f5e2815a0437c96479a2ab27814126c61765791e
FraPazGal <fdepaz@vmware.com>
2022-10-21 09:39:28 +0200

    Improve ingress related tests
    Signed-off-by: FraPazGal <fdepaz@vmware.com>

--- .vib/contour/ginkgo/contour.go
+++ .vib/contour/ginkgo/contour.go
@@ -6,10 +6,7 @@ import (
 	. "github.com/onsi/ginkgo/v2"
 	. "github.com/onsi/gomega"
 
-	v1 "k8s.io/api/core/v1"
 	netv1 "k8s.io/api/networking/v1"
-	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
-	cv1 "k8s.io/client-go/kubernetes/typed/core/v1"
 	netcv1 "k8s.io/client-go/kubernetes/typed/networking/v1"
 
 	// For client auth plugins
@@ -18,58 +15,22 @@ import (
 
 var _ = Describe("Contour", func() {
 	var netclient netcv1.NetworkingV1Interface
-	var coreclient cv1.CoreV1Interface
 	var ctx context.Context
 
 	BeforeEach(func() {
 		netclient = netcv1.NewForConfigOrDie(clusterConfigOrDie())
-		coreclient = cv1.NewForConfigOrDie(clusterConfigOrDie())
 		ctx = context.Background()
 	})
 
-	Context("Creating example ingress", func() {
-		var ingress netv1.Ingress
+	Context("Testing ingress", func() {
+		var ingresses netv1.IngressList
 
 		BeforeEach(func() {
-			var pathType netv1.PathType = "Prefix"
-
-			ingressRuleValue := netv1.IngressRuleValue{
-				HTTP: &netv1.HTTPIngressRuleValue{
-					Paths: []netv1.HTTPIngressPath{
-						{
-							Path:     "/",
-							PathType: &pathType,
-							Backend: netv1.IngressBackend{
-								Service: &netv1.IngressServiceBackend{
-									Name: "example-service",
-									Port: netv1.ServiceBackendPort{
-										Number: 80,
-									},
-								},
-							},
-						},
-					},
-				},
-			}
-			ingress = createIngressOrDie(ctx, netclient, *namespace, "test-ingress", "www.example.com", ingressRuleValue)
+			ingresses = getIngressesByLabelOrDie(ctx, netclient, *namespace, "app=kuard")
 		})
 
-		When("the example ingress is created", func() {
-			var controllerPods v1.PodList
-			var podLogs []string
-
-			BeforeEach(func() {
-				controllerPods = getPodsByLabelOrDie(ctx, coreclient, *namespace, "app.kubernetes.io/component=contour")
-				podLogs = getPodLogsOrDie(ctx, coreclient, *namespace, controllerPods.Items[0].GetName(), "contour")
-			})
-
-			It("is managed by contour", func() {
-				Expect(*ingress.Spec.IngressClassName).To(Equal("contour"))
-				Expect(containsString(podLogs, ingress.GetName())).To(BeTrue())
-			})
-		})
-		AfterEach(func() {
-			netclient.Ingresses(*namespace).Delete(ctx, ingress.GetName(), metav1.DeleteOptions{})
+		It("is managed by contour", func() {
+			Expect(*ingresses.Items[0].Spec.IngressClassName).To(Equal("contour"))
 		})
 	})
 })
--- .vib/contour/ginkgo/integration_suite_test.go
+++ .vib/contour/ginkgo/integration_suite_test.go
@@ -1,18 +1,13 @@
 package integration
 
 import (
-	"bufio"
 	"context"
 	"flag"
 	"fmt"
-	"io"
-	"strings"
 	"testing"
 
-	v1 "k8s.io/api/core/v1"
 	netv1 "k8s.io/api/networking/v1"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
-	cv1 "k8s.io/client-go/kubernetes/typed/core/v1"
 	netcv1 "k8s.io/client-go/kubernetes/typed/networking/v1"
 	"k8s.io/client-go/rest"
 	"k8s.io/client-go/tools/clientcmd"
@@ -28,6 +23,7 @@ const APP_NAME = "Contour"
 
 var kubeconfig = flag.String("kubeconfig", "", "absolute path to the kubeconfig file")
 var namespace = flag.String("namespace", "", "namespace where the resources are deployed")
+var ingressName = flag.String("ingress-name", "", "resource name of the testing ingress")
 
 func clusterConfigOrDie() *rest.Config {
 	var config *rest.Config
@@ -45,98 +41,25 @@ func clusterConfigOrDie() *rest.Config {
 	return config
 }
 
-func createIngressOrDie(ctx context.Context, c netcv1.NetworkingV1Interface, namespace string, name string, ingressRuleHost string, ingressRuleValue netv1.IngressRuleValue) netv1.Ingress {
-	ingress := &netv1.Ingress{
-		ObjectMeta: metav1.ObjectMeta{
-			Namespace: namespace,
-			Name:      name,
-		},
-		Spec: netv1.IngressSpec{
-			Rules: []netv1.IngressRule{
-				{
-					Host:             ingressRuleHost,
-					IngressRuleValue: ingressRuleValue,
-				},
-			},
-		},
-	}
+func getIngressesByLabelOrDie(ctx context.Context, c netcv1.NetworkingV1Interface, namespace string, selector string) netv1.IngressList {
 
-	result, err := c.Ingresses(namespace).Create(ctx, ingress, metav1.CreateOptions{})
-	if err != nil {
-		panic(err.Error())
-	}
-	fmt.Printf("Created ingress %q.\n", result.GetObjectMeta().GetName())
-
-	return *result
-}
-
-func getPodsByLabelOrDie(ctx context.Context, c cv1.PodsGetter, namespace string, selector string) v1.PodList {
-
-	output, err := c.Pods(namespace).List(ctx, metav1.ListOptions{
+	output, err := c.Ingresses(namespace).List(ctx, metav1.ListOptions{
 		LabelSelector: selector,
 	})
 	if err != nil {
 		panic(err.Error())
 	}
-	fmt.Printf("Obtained list of pods with label %q\n", selector)
+	fmt.Printf("Obtained list of ingresses with label %q\n", selector)
 
 	return *output
-}
-
-func getPodLogsOrDie(ctx context.Context, c cv1.PodsGetter, namespace, podName, container string) []string {
-	var output []string
-	tailLines := int64(5)
-
-	readCloser, err := c.Pods(namespace).GetLogs(podName, &v1.PodLogOptions{
-		Container: container,
-		Follow:    false,
-		TailLines: &tailLines,
-	}).Stream(ctx)
-	if err != nil {
-		panic(err.Error())
-	}
-	fmt.Printf("Obtained %q pod's logs\n", podName)
-
-	defer readCloser.Close()
-
-	scanner := bufio.NewScanner(interruptableReader{ctx, readCloser})
-	for scanner.Scan() {
-		output = append(output, scanner.Text())
-	}
-	if scanner.Err() != nil {
-		panic(scanner.Err())
-	}
-	return output
-}
-
-type interruptableReader struct {
-	ctx context.Context
-	r   io.Reader
-}
-
-func (r interruptableReader) Read(p []byte) (int, error) {
-	if err := r.ctx.Err(); err != nil {
-		return 0, err
-	}
-	n, err := r.r.Read(p)
-	if err != nil {
-		return n, err
-	}
-	return n, r.ctx.Err()
-}
-
-func containsString(haystack []string, needle string) bool {
-	for _, s := range haystack {
-		if strings.Contains(s, needle) {
-			return true
-		}
-	}
-	return false
 }
 
 func CheckRequirements() {
 	if *namespace == "" {
 		panic(fmt.Sprintf("The namespace where %s is deployed must be provided. Use the '--namespace' flag", APP_NAME))
+	}
+	if *ingressName == "" {
+		panic("The resource name of the testing ingress. Use the '--ingress-name' flag")
 	}
 }
 
