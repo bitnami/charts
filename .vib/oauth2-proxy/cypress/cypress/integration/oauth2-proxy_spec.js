c46dca220fc873f66d9650a84993969158cbe823
Jose Antonio Carmona <jcarmona@vmware.com>
2022-11-11 14:17:51 +0100

    Add explanations on Cypress tests
    Signed-off-by: Jose Antonio Carmona <jcarmona@vmware.com>

--- .vib/oauth2-proxy/cypress/cypress/integration/oauth2-proxy_spec.js
+++ .vib/oauth2-proxy/cypress/cypress/integration/oauth2-proxy_spec.js
@@ -1,9 +1,16 @@
 /// <reference types="cypress" />
 
 it('redirects to the auth provider login', () => {
+  // Access to the URL without proper identification results in a forbidden response
+  // with the UI to log in.
   cy.visit('/', {failOnStatusCode:false})
   cy.contains('button', 'Sign in');
 
+  // Unfortunately, a third-party auth provider service (like Google, Facebook, OpenID connector, ...)
+  // is needed to test the identification process, which is difficult to configure for the tests.
+  //
+  // Hence, the test focuses on asserting that a redirection to the default auth provider (Google) is
+  // performed.
   cy.request({
     url: "/oauth2/start",
     method: "GET"
