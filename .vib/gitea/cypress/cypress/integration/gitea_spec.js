cfbf4108d19fff22a6b80c5ebf2992b6ad8ad231
Javier Salmeron Garcia <jsalmeron@vmware.com>
2022-11-24 16:10:51 +0100

    [bitnami/gitea] feat: :tada: Add chart
    Signed-off-by: Javier Salmeron Garcia <jsalmeron@vmware.com>

---
+++ .vib/gitea/cypress/cypress/integration/gitea_spec.js
@@ -0,0 +1,16 @@
+/// <reference types="cypress" />
+import {
+  random,
+} from '../support/utils';
+
+it.only('allows to create a git repository', () => {
+  cy.login();
+  cy.visit('/repo/create');
+  cy.fixture('repo').then((repo) => {
+    cy.get('input[name="repo_name"]').type(`${repo.name}-${random}`, {
+      force: true,
+    });
+  });
+  cy.get('button.green').click();
+  cy.contains('touch README.md');
+});
