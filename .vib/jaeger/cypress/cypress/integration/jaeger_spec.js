e3196674e09817c2b8ab0a32ffb04ff6b7623398
Alberto Otero Lorenzo <lorenzoa@vmware.com>
2022-12-05 12:23:52 +0100

    Update tests
    Signed-off-by: Alberto Otero Lorenzo <lorenzoa@vmware.com>

--- .vib/jaeger/cypress/cypress/integration/jaeger_spec.js
+++ .vib/jaeger/cypress/cypress/integration/jaeger_spec.js
@@ -1,28 +1,29 @@
 /// <reference types="cypress" />
 
-it('list and retrieve jaeger traces', async () => {
+it('list and retrieve jaeger traces', () => {
 
     const testService = 'redis';
     // Check application availability though the UI
-    await cy.visit('/search?end=3669306189365000&limit=20&lookback=1h&maxDuration&minDuration&service=' + testService + '&start=0');
+    cy.visit('/search?end=3669306189365000&limit=20&lookback=1h&maxDuration&minDuration&service=' + testService + '&start=0').then(() => {
 
-    // Ensure page contains Traces in an H2 tag
-    await cy.contains('h2', 'Traces');
+        // Ensure page contains Traces in an H2 tag
+        cy.contains('h2', 'Traces');
 
-    let traceIDLink = '';
-    // Get <a> tag link of class "ResultItemTitle--item ub-flex-auto"
-    cy.get('a.ResultItemTitle--item.ub-flex-auto').then((href) => {
-        traceIDLink = href[0].href;
-        // Get traceIDLink trace id after last slash
-        const traceID = traceIDLink.substring(traceIDLink.lastIndexOf('/') + 1, traceIDLink.length);
+        let traceIDLink = '';
+        // Get <a> tag link of class "ResultItemTitle--item ub-flex-auto"
+        cy.get('a.ResultItemTitle--item.ub-flex-auto').then((href) => {
+            traceIDLink = href[0].href;
+            // Get traceIDLink trace id after last slash
+            const traceID = traceIDLink.substring(traceIDLink.lastIndexOf('/') + 1, traceIDLink.length);
 
-        // Get trace info through API
-        cy.request({
-            method: 'GET',
-            url: '/api/traces/' + traceID
-        }).then((response) => {
-            expect(response.status).to.eq(200);
-            expect(response.body.data[0].traceID).to.eq(traceID);
-        });
-    })
+            // Get trace info through API
+            cy.request({
+                method: 'GET',
+                url: '/api/traces/' + traceID
+            }).then((response) => {
+                expect(response.status).to.eq(200);
+                expect(response.body.data[0].traceID).to.eq(traceID);
+            });
+        })
+    });
 });
