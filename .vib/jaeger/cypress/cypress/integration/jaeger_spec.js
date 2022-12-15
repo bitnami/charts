10e62b39860300df4f1971d51490d14cdc6f2942
Alberto Otero Lorenzo <lorenzoa@vmware.com>
2022-12-15 12:53:53 +0100

    Indent
    Signed-off-by: Alberto Otero Lorenzo <lorenzoa@vmware.com>

--- .vib/jaeger/cypress/cypress/integration/jaeger_spec.js
+++ .vib/jaeger/cypress/cypress/integration/jaeger_spec.js
@@ -2,27 +2,27 @@
 
 it('lists and retrieves jaeger traces', () => {
 
-    const testService = 'redis';
-    const currentDate = new Date();
-    const timestampMillis = currentDate.getTime() * 1000;
+  const testService = 'redis';
+  const currentDate = new Date();
+  const timestampMillis = currentDate.getTime() * 1000;
 
-    cy.visit(`/search?end=${timestampMillis}&limit=20&lookback=1h&maxDuration&minDuration&service=${testService}&start=0`)
+  cy.visit(`/search?end=${timestampMillis}&limit=20&lookback=1h&maxDuration&minDuration&service=${testService}&start=0`)
 
-    // Ensure page contains Traces in an H2 tag
-    cy.contains('h2', 'Traces');
+  // Ensure page contains Traces in an H2 tag
+  cy.contains('h2', 'Traces');
 
-    let traceIDLink = '';
-    cy.get('a.ResultItemTitle--item.ub-flex-auto').then((href) => {
-        traceIDLink = href[0].href;
-        const traceID = traceIDLink.substring(traceIDLink.lastIndexOf('/') + 1, traceIDLink.length);
+  let traceIDLink = '';
+  cy.get('a.ResultItemTitle--item.ub-flex-auto').then((href) => {
+    traceIDLink = href[0].href;
+    const traceID = traceIDLink.substring(traceIDLink.lastIndexOf('/') + 1, traceIDLink.length);
 
-        cy.request({
-            method: 'GET',
-            url: '/api/traces/' + traceID
-        }).then((response) => {
-            expect(response.status).to.eq(200);
-            expect(response.body.data[0].traceID).to.eq(traceID);
-        });
-    })
+    cy.request({
+      method: 'GET',
+      url: '/api/traces/' + traceID
+    }).then((response) => {
+      expect(response.status).to.eq(200);
+      expect(response.body.data[0].traceID).to.eq(traceID);
+    });
+  })
 
 });
