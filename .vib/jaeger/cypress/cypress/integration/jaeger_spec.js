c00b49feb71b33e9f6fd82e106f955b34af875a5
Alberto Otero Lorenzo <lorenzoa@vmware.com>
2022-12-15 13:47:35 +0100

    GOSS fixes
    Signed-off-by: Alberto Otero Lorenzo <lorenzoa@vmware.com>

--- .vib/jaeger/cypress/cypress/integration/jaeger_spec.js
+++ .vib/jaeger/cypress/cypress/integration/jaeger_spec.js
@@ -2,14 +2,12 @@
 
 it('lists and retrieves jaeger traces', () => {
 
+  // The container we use to create traces, usea 'redis' as a service name
   const testService = 'redis';
   const currentDate = new Date();
   const timestampMillis = currentDate.getTime() * 1000;
 
-  cy.visit(`/search?end=${timestampMillis}&limit=20&lookback=1h&maxDuration&minDuration&service=${testService}&start=0`)
-
-  // Ensure page contains Traces in an H2 tag
-  cy.contains('h2', 'Traces');
+  cy.visit(`/search?end=${timestampMillis}&limit=20&lookback=1h&maxDuration&minDuration&service=${testService}&start=0`);
 
   cy.get('a.ResultItemTitle--item.ub-flex-auto').invoke('attr', 'href').then((href) => {
     const traceID = href.substring(href.lastIndexOf('/') + 1, href.length);
