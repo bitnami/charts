e2a162c9c7f73d34741dd7c4fd3602245f7ff040
Cori Avila <amisericordi@vmware.com>
2023-01-09 18:12:57 +0100

    [bitnami/sonarquibe] Added exception handler
    Signed-off-by: Cori Avila <amisericordi@vmware.com>

--- .vib/sonarqube/cypress/cypress/support/commands.js
+++ .vib/sonarqube/cypress/cypress/support/commands.js
@@ -21,3 +21,14 @@ Cypress.Commands.add(
     cy.contains('button', 'Log in').click();
   }
 );
+
+Cypress.on('uncaught:exception', (err, runnable, promise) => {
+  // when the exception originated from an unhandled promise
+  // rejection, the promise is provided as a third argument
+  // you can turn off failing the test in this case
+  if (promise) {
+    return false
+  }
+  // we still want to ensure there are no other unexpected
+  // errors, so we let them fail the test
+})
