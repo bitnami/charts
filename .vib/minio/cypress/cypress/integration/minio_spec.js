c61afdd119e1cf98455fdd164e9b19ac5f6527d7
David Gomez <dgomezleon@vmware.com>
2023-01-18 13:16:21 +0100

    [bitnami/minio] Fix cypress test
    Signed-off-by: David Gomez <dgomezleon@vmware.com>

--- .vib/minio/cypress/cypress/integration/minio_spec.js
+++ .vib/minio/cypress/cypress/integration/minio_spec.js
@@ -8,8 +8,9 @@ it('allows creating a bucket, uploading and retrieving a file', () => {
     bucketName=`${buckets.newBucket.name}.${random}`;
     cy.get('#bucket-name').type(`${bucketName}`);
     cy.contains('button', 'Create Bucket').click();
+    cy.get('a[href="/browser"]').click();
+    cy.get(`span[id="browse-${bucketName}"]`).click();
   });
-
 
   const fileToUpload = 'example.json';
   cy.get('#upload-main').click();
