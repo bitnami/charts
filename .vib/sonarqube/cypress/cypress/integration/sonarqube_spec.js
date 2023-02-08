04f04d5ed3602843e1143fadf65ad88e6177d7b2
Rafael Rios Saavedra <rrios@vmware.com>
2023-02-08 10:39:50 +0100

    [bitnami/sonarqube] Fix VIB test for sonarqube verion 9.9
    Signed-off-by: Rafael Rios Saavedra <rrios@vmware.com>

--- .vib/sonarqube/cypress/cypress/integration/sonarqube_spec.js
+++ .vib/sonarqube/cypress/cypress/integration/sonarqube_spec.js
@@ -18,9 +18,11 @@ it('allows adding a project and a quality gate', () => {
       cy.contains('Create').click();
       cy.get('#quality-gate-form-name').type(`${qualityGates.newQualityGate.name}${random}`);
       cy.get('[type="submit"]').contains('Save').click();
+
+      cy.contains('Unlock editing').click();
       cy.contains('Add Condition').click();
       cy.get('#condition-metric').click();
-      cy.contains('Lines').click({force: true});
+      cy.contains('Lines to Cover').click({force: true});
       cy.get('#condition-threshold').type(qualityGates.newQualityGate.threshold);
       cy.get('[type="submit"]').click();
 
