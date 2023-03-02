bcc5e04b2dc826e808733fabc662b563cb0589eb
FraPazGal <fdepaz@vmware.com>
2023-03-02 10:45:33 +0100

    Fix tests structure
    Signed-off-by: FraPazGal <fdepaz@vmware.com>

--- .vib/contour-operator/ginkgo/contour_operator_test.go
+++ .vib/contour-operator/ginkgo/contour_operator_test.go
@@ -19,28 +19,26 @@ import (
 )
 
 var _ = Describe("Contour Operator:", func() {
-	var netclient netcv1.NetworkingV1Interface
-	var coreclient cv1.CoreV1Interface
-	var dynamicClient dynamic.Interface
-	var ctx context.Context
-
-	BeforeEach(func() {
-		netclient = netcv1.NewForConfigOrDie(clusterConfigOrDie())
-		coreclient = cv1.NewForConfigOrDie(clusterConfigOrDie())
-		dynamicClient = dynamic.NewForConfigOrDie(clusterConfigOrDie())
-		ctx = context.Background()
-	})
-
-	Context("When both operator and testing resources are deployed", func() {
-		var ingressHost string
+	Context("When both operator and testing resources are deployed", Ordered, func() {
+		var netclient netcv1.NetworkingV1Interface
+		var coreclient cv1.CoreV1Interface
+		var dynamicClient dynamic.Interface
 		var envoySvc *v1.Service
-		var hasIP, isReady bool
+		var ctx context.Context
 		var err error
 
 		const contourName = "contour-crd-vib"
 		const ingressName = "contour-ing-vib"
 
 		BeforeAll(func() {
+			var ingressHost string
+			var hasIP, isReady bool
+
+			netclient = netcv1.NewForConfigOrDie(clusterConfigOrDie())
+			coreclient = cv1.NewForConfigOrDie(clusterConfigOrDie())
+			dynamicClient = dynamic.NewForConfigOrDie(clusterConfigOrDie())
+			ctx = context.Background()
+
 			// The tests evaluate the Operator by deploying both Contour and Ingress resources.
 			// Their creation takes some time, so they will be deployed once and reused across the different checks.
 			createContourResourceOrDie(ctx, dynamicClient, contourName)
@@ -88,8 +86,8 @@ var _ = Describe("Contour Operator:", func() {
 				panic(fmt.Sprintf("There was an error retrieving the %q Ingress resource: %q", ingressName, err))
 			}
 			Expect(returnValidHost(testingIngress.Status.LoadBalancer.Ingress[0])).To(Equal(returnValidHost(envoySvc.Status.LoadBalancer.Ingress[0])))
-
 		})
+
 		AfterAll(func() {
 			// No need to panic here if failed, the cluster is expected to clean up with the undeployment
 			dynamicClient.Resource(contourType).Namespace(*namespace).Delete(ctx, contourName, metav1.DeleteOptions{})
