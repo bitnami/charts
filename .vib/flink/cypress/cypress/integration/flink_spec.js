86bd18a5c220323bcee527748c37d8d06d608d15
Alberto Otero Lorenzo <lorenzoa@vmware.com>
2023-03-20 15:44:56 +0100

    Update Cypress tests
    Signed-off-by: Alberto Otero Lorenzo <lorenzoa@vmware.com>

--- .vib/flink/cypress/cypress/integration/flink_spec.js
+++ .vib/flink/cypress/cypress/integration/flink_spec.js
@@ -8,16 +8,19 @@ describe.only('Apache Flink', () => {
       .should('eq', 200)
   })
 
+  it('should show available task slots', () => {
+    cy.visit('/')
+    cy.get('div.field').within(() => {
+      cy.contains('Total Task Slots'); // Asserts that the first span contains the text "Total Task Slots"
+      cy.contains('span', /^\d+$/).should(($span) => {
+        const num = parseInt($span.text());
+        expect(num).to.be.greaterThan(0); // Asserts that the number in the second span is greater than 0
+      });
+    });
+  })
+
   it('should show completed jobs', () => {
     cy.visit('/')
     cy.contains('span', 'FINISHED')
-  })
-
-  it('should show available task slots', () => {
-    cy.visit('/')
-    cy.get('.field').within(() => {
-      cy.contains('Total Task Slots');
-      cy.contains('6');
-    });
   })
 })
