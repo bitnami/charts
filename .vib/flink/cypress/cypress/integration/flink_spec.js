5d72183a1133918c7c1436de36812e0c903e2ebb
Alberto Otero Lorenzo <lorenzoa@vmware.com>
2023-03-21 17:27:45 +0100

    Test improvements
    Signed-off-by: Alberto Otero Lorenzo <lorenzoa@vmware.com>

--- .vib/flink/cypress/cypress/integration/flink_spec.js
+++ .vib/flink/cypress/cypress/integration/flink_spec.js
@@ -1,26 +1,18 @@
 /// <reference types="cypress" />
 
-describe.only('Apache Flink', () => {
-  it('Flink UI should return 200', () => {
-    cy.visit('/')
-    cy.request('/')
-      .its('status')
-      .should('eq', 200)
-  })
+it('should show available task slots', () => {
+  cy.visit('/')
 
-  it('should show available task slots', () => {
-    cy.visit('/')
-    cy.get('div.field').within(() => {
-      cy.contains('Total Task Slots'); // Asserts that the first span contains the text "Total Task Slots"
-      cy.contains('span', /^\d+$/).should(($span) => {
-        const num = parseInt($span.text());
-        expect(num).to.be.greaterThan(0); // Asserts that the number in the second span is greater than 0
-      });
+  cy.get('div.field', 'Total Task Slots').within(() => {
+    cy.contains('span', /^\d+$/).should(($span) => {
+      const num = parseInt($span.text());
+      expect(num).to.be.greaterThan(0); // Asserts that the number in the second span is greater than 0
     });
-  })
-
-  it('should show completed jobs', () => {
-    cy.visit('/')
-    cy.contains('span', 'FINISHED')
-  })
+  });
 })
+
+it('should show completed jobs', () => {
+  cy.visit('/')
+  cy.contains('span', 'FINISHED')
+})
+
