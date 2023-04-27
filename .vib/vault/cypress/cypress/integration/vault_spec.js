2f5fbada63aa6ffd67abacf0db3837e163f4fc72
Javier Salmeron Garcia <jsalmeron@vmware.com>
2023-04-27 19:00:37 +0200

    test: Remove unnecessary fixture
    Signed-off-by: Javier Salmeron Garcia <jsalmeron@vmware.com>

--- .vib/vault/cypress/cypress/integration/vault_spec.js
+++ .vib/vault/cypress/cypress/integration/vault_spec.js
@@ -8,7 +8,7 @@ it('allows to use the API Explorer', () => {
   // Go to the secrets page
   cy.get('button[class*="nav-console"]').click();
   // Create a secret
-  cy.fixture('secret').then((op) => {
+  cy.fixture('operation').then((op) => {
     cy.get('input').type(`${op.operation.command}{enter}`);
     cy.get('pre').contains(`${op.operation.expectedOutput}`).should('be.visible');
   });
