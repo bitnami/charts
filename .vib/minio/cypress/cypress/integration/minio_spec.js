fa1c21ea6d05ccc58d1f4fc6f44fe8d469c2b0ad
Fran Mulero <fmulero@vmware.com>
2023-05-24 17:00:38 +0200

    Fix Upload test
    Signed-off-by: Fran Mulero <fmulero@vmware.com>

--- .vib/minio/cypress/cypress/integration/minio_spec.js
+++ .vib/minio/cypress/cypress/integration/minio_spec.js
@@ -13,7 +13,8 @@ it('allows creating a bucket, uploading and retrieving a file', () => {
 
   const fileToUpload = 'example.json';
   cy.get('#upload-main').click();
-  cy.contains('Upload File').click();
+  // Once the main button is clicked a tooltip overlaps with 'Upload File' button
+  cy.contains('Upload File').click({ force: true });
   cy.get('#object-list-wrapper').within(() => {
     cy.get('[type="file"]')
       .should('not.be.disabled')
