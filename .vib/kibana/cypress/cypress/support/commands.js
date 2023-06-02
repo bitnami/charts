0185704d21eb7e5f604ef12da1bab80fd89fab9b
Cori Avila <amisericordi@vmware.com>
2023-06-02 11:16:00 +0200

    [bitnami/kibana] Add new condition
    Signed-off-by: Cori Avila <amisericordi@vmware.com>

--- .vib/kibana/cypress/cypress/support/commands.js
+++ .vib/kibana/cypress/cypress/support/commands.js
@@ -21,7 +21,12 @@ Cypress.Commands.add('forceClick', { prevSubject: 'element' }, (subject) => {
 Cypress.on('uncaught:exception', (err) => {
   // We expect an error "Cannot read properties of undefined (reading 'includes')"
   // during the installation of a template so we add an exception
-  return false;
+  if (err.message.includes("Cannot read properties of undefined (reading 'includes')")) {
+    return false;
+  }
+  if(err.message.includes("ResizeObserver loop limit exceeded")){
+    return false;
+  }
   // we still want to ensure there are no other unexpected
   // errors, so we let them fail the test
 })
