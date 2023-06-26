4222d31230853074dda029e6d46bfec1497bca65
David Gomez <dgomezleon@vmware.com>
2023-06-26 13:10:05 +0200

    [bitnami/vault] Fix Cypress tests
    Signed-off-by: David Gomez <dgomezleon@vmware.com>

--- .vib/vault/cypress/cypress/integration/vault_spec.js
+++ .vib/vault/cypress/cypress/integration/vault_spec.js
@@ -6,7 +6,7 @@ import {
 it('allows to use the API Explorer', () => {
   cy.login();
   // Go to the secrets page
-  cy.get('button[class*="nav-console"]').click();
+  cy.get('button[aria-label*="Console"]').click();
   // Create a secret
   cy.fixture('operation').then((op) => {
     cy.get('input').type(`${op.operation.command}{enter}`);
