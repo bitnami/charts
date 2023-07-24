a52aed7bfc0a1cc23e7c77801f41453ce9b77696
Celia Garcia Marquez <gcelia@vmware.com>
2023-07-24 13:28:23 +0200

    [bitnami/opencart] Omit not allowed character for keyword
    Signed-off-by: Celia Garcia Marquez <gcelia@vmware.com>

--- .vib/opencart/cypress/cypress/integration/opencart_spec.js
+++ .vib/opencart/cypress/cypress/integration/opencart_spec.js
@@ -48,7 +48,7 @@ it('allows an admin to add a product to the catalog', () => {
     cy.get('#input-meta-title-1').type(`${product.newProduct.metatagTitle}.${random}`);
     cy.contains('Data').click();
     cy.get('#input-model').type(`${product.newProduct.model}.${random}`);
-    cy.get('#input-keyword-0-1').type(`${product.newProduct.model}.${random}`, { force: true });
+    cy.get('#input-keyword-0-1').type(`${product.newProduct.model}${random}`, { force: true });
     cy.get('[title="Save"]').click();
     cy.contains('Success');
     cy.visit('/');
