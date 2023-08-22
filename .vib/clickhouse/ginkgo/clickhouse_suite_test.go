f38f3bcfb148ee9079fdbe9d07f9fc5836edff93
Javier Salmeron Garcia <jsalmeron@vmware.com>
2023-08-22 12:39:26 +0200

    [bitnami/clickhouse] test: :white_check_mark: Add persistence tests
    Signed-off-by: Javier Salmeron Garcia <jsalmeron@vmware.com>

---
+++ .vib/clickhouse/ginkgo/clickhouse_suite_test.go
@@ -0,0 +1,21 @@
+package clickhouse_test
+
+import (
+	"flag"
+	"testing"
+
+	. "github.com/onsi/ginkgo/v2"
+	. "github.com/onsi/gomega"
+)
+
+var kubeconfig = flag.String("kubeconfig", "", "absolute path to the kubeconfig file")
+var releaseName = flag.String("name", "", "name of the primary statefulset")
+var namespace = flag.String("namespace", "", "namespace where the application is running")
+var username = flag.String("username", "", "database user")
+var password = flag.String("password", "", "database password for username")
+var shards = flag.Int("shards", 3, "number of shards")
+
+func TestMariaDB(t *testing.T) {
+	RegisterFailHandler(Fail)
+	RunSpecs(t, "ClickHouse Persistence Test Suite")
+}
