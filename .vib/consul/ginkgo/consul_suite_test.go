03475db7a615dbfb3eff57356e2327ba3e3868ce
Javier Salmeron Garcia <jsalmeron@vmware.com>
2023-08-22 16:08:31 +0200

    [bitnami/consul] test: :white_check_mark: Add persistence test
    Signed-off-by: Javier Salmeron Garcia <jsalmeron@vmware.com>

---
+++ .vib/consul/ginkgo/consul_suite_test.go
@@ -0,0 +1,18 @@
+package consul_test
+
+import (
+	"flag"
+	"testing"
+
+	. "github.com/onsi/ginkgo/v2"
+	. "github.com/onsi/gomega"
+)
+
+var kubeconfig = flag.String("kubeconfig", "", "absolute path to the kubeconfig file")
+var stsName = flag.String("name", "", "name of the primary statefulset")
+var namespace = flag.String("namespace", "", "namespace where the application is running")
+
+func TestMariaDB(t *testing.T) {
+	RegisterFailHandler(Fail)
+	RunSpecs(t, "Consul Persistence Test Suite")
+}
