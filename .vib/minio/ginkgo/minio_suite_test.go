7c0a06cfbae9076ac783ebea3c7ca7a49a8b7903
Javier Salmeron Garcia <jsalmeron@vmware.com>
2023-08-23 12:47:57 +0200

    [bitnami/minio] test: :white_check_mark: Add persistence tests
    Signed-off-by: Javier Salmeron Garcia <jsalmeron@vmware.com>

---
+++ .vib/minio/ginkgo/minio_suite_test.go
@@ -0,0 +1,20 @@
+package minio_test
+
+import (
+	"flag"
+	"testing"
+
+	. "github.com/onsi/ginkgo/v2"
+	. "github.com/onsi/gomega"
+)
+
+var kubeconfig = flag.String("kubeconfig", "", "absolute path to the kubeconfig file")
+var deployName = flag.String("name", "", "name of the primary statefulset")
+var namespace = flag.String("namespace", "", "namespace where the application is running")
+var username = flag.String("username", "", "database user")
+var password = flag.String("password", "", "database password for username")
+
+func TestMinio(t *testing.T) {
+	RegisterFailHandler(Fail)
+	RunSpecs(t, "Minio Persistence Test Suite")
+}
