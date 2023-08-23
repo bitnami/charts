0beea9bf1746af79fbcfe9be05d4192c138b1316
Javier Salmeron Garcia <jsalmeron@vmware.com>
2023-08-23 14:59:36 +0200

    [bitnami/redis] test: :white_check_mark: Add persistence tests
    Signed-off-by: Javier Salmeron Garcia <jsalmeron@vmware.com>

---
+++ .vib/redis/ginkgo/redis_suite_test.go
@@ -0,0 +1,19 @@
+package redis_test
+
+import (
+	"flag"
+	"testing"
+
+	. "github.com/onsi/ginkgo/v2"
+	. "github.com/onsi/gomega"
+)
+
+var kubeconfig = flag.String("kubeconfig", "", "absolute path to the kubeconfig file")
+var stsName = flag.String("name", "", "name of the primary statefulset")
+var namespace = flag.String("namespace", "", "namespace where the application is running")
+var password = flag.String("password", "", "password for the installation")
+
+func TestRedis(t *testing.T) {
+	RegisterFailHandler(Fail)
+	RunSpecs(t, "Redis Persistence Test Suite")
+}
