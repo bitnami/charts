49fd43a6ad76e4f13e66c8e0cdf40e7af5067e1a
Miguel Ruiz <miruiz@vmware.com>
2023-09-27 12:36:46 +0200

    Fix BASE_URL again
    Signed-off-by: Miguel Ruiz <miruiz@vmware.com>

--- .vib/suitecrm/cypress/cypress/support/commands.js
+++ .vib/suitecrm/cypress/cypress/support/commands.js
@@ -4,7 +4,7 @@
  */
 
 const COMMAND_DELAY = 2000;
-const BASE_URL = 'http://vmware-suitecrm.my/';
+const BASE_URL = 'http://vmware-suitecrm.my';
 
 for (const command of ['click']) {
   Cypress.Commands.overwrite(command, (originalFn, ...args) => {
