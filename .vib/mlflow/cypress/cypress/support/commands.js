83dcaaec37e2feae507a5aa3396e060df6edcf4f
Javier Salmeron Garcia <jsalmeron@vmware.com>
2023-10-09 10:41:53 +0200

    test: :white_check_mark: Add exception handling
    Signed-off-by: Javier Salmeron Garcia <jsalmeron@vmware.com>

--- .vib/mlflow/cypress/cypress/support/commands.js
+++ .vib/mlflow/cypress/cypress/support/commands.js
@@ -17,3 +17,13 @@ for (const command of ['click']) {
   });
 }
 
+Cypress.on('uncaught:exception', (err, runnable, promise) => {
+  // when the exception originated from an unhandled promise
+  // rejection, the promise is provided as a third argument
+  // you can turn off failing the test in this case
+  if (promise) {
+    return false
+  }
+  // we still want to ensure there are no other unexpected
+  // errors, so we let them fail the test
+})
