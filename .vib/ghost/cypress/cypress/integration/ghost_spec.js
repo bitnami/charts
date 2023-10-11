208878de27e233db34ec064801e9045fcd8a73ce
mdhont <michield@vmware.com>
2023-10-11 09:28:40 +0200

    Fix indentation
    Signed-off-by: mdhont <michield@vmware.com>

--- .vib/ghost/cypress/cypress/integration/ghost_spec.js
+++ .vib/ghost/cypress/cypress/integration/ghost_spec.js
@@ -9,7 +9,7 @@ import { random } from '../support/utils';
 it('allows to create and publish a new post with an image', () => {
   cy.login();
   cy.visit('/ghost/#/posts');
-    cy.get('span').contains('New post').click();
+  cy.get('span').contains('New post').click();
   cy.fixture('posts').then((posts) => {
     cy.get('textarea[placeholder="Post title"]').type(
       `${posts.newPost.title}-${random}`
