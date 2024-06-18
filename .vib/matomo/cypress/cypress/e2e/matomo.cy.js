2afb92099ff16b4a7eef452fc0fd0f5b3a3d2848
Miguel Ruiz <miruiz@vmware.com>
2024-06-18 17:24:16 +0200

    Update tests
    Signed-off-by: Miguel Ruiz <miruiz@vmware.com>

--- .vib/matomo/cypress/cypress/e2e/matomo.cy.js
+++ .vib/matomo/cypress/cypress/e2e/matomo.cy.js
@@ -29,17 +29,10 @@ it('allows to create a new website', () => {
 // The Matomo API allows checking the site analytics and tracking metrics
 // Source: https://matomo.org/guide/apis/analytics-api/
 it('allows to use the API to retrieve analytics', () => {
-  // Record a new visit in order to generate analytics beforehand
-  cy.request(`${baseURL()}/matomo.php?idsite=1&rec=1`).then((response) => {
-    expect(response.status).to.eq(200);
-  });
-
   cy.login();
   // Navigate using the UI as Matomo will randomly fail with
   // "token mismatch" if accessed directly
-  cy.contains('Forms', {timeout: 60000});
   cy.get('#topmenu-coreadminhome').click();
-  cy.contains('System Summary', {timeout: 60000});
   cy.contains('Personal').click();
   cy.contains('Security').click();
   cy.contains('Create new token', {timeout: 60000}).click();
@@ -49,6 +42,12 @@ it('allows to use the API to retrieve analytics', () => {
   cy.get('input[id="secure_only"]').click({ force: true });
   cy.get('[type="submit"]').click();
   cy.contains('Token successfully generated', {timeout: 60000});
+  // Record a new visit in order to generate analytics beforehand
+  cy.request(`${baseURL()}/matomo.php?idsite=1&rec=1`).then((response) => {
+    expect(response.status).to.eq(200);
+  });
+  cy.wait(1000);
+  // Get the visit information using the API token
   cy.get('code')
     .invoke('text')
     .then((apiToken) => {
