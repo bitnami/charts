894ae5599e59627ec2da6dd61db07604029b9215
Miguel Ruiz <miruiz@vmware.com>
2024-06-18 18:10:00 +0200

    Use baseUrl
    Signed-off-by: Miguel Ruiz <miruiz@vmware.com>

--- .vib/matomo/cypress/cypress.config.js
+++ .vib/matomo/cypress/cypress.config.js
@@ -5,11 +5,9 @@ module.exports = {
     email: 'user@example.com',
     port: '80',
   },
-  hosts: {
-    'bitnami-matomo.my': '{{ TARGET_IP }}',
-  },
   defaultCommandTimeout: 30000,
   e2e: {
     setupNodeEvents(on, config) {},
+    baseUrl: 'http://localhost',
   },
 }
