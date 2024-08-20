9f594f8e264610ab436cf328d011720047a6393e
Javier J. Salmerón García <javier.salmeron@broadcom.com>
2024-08-20 16:04:36 +0200

    [bitnami/argo-cd] test: :white_check_mark: Improve resilience of cypress test
    Signed-off-by: Javier J. Salmerón García <javier.salmeron@broadcom.com>

--- .vib/argo-cd/cypress/cypress/e2e/argo_cd.cy.js
+++ .vib/argo-cd/cypress/cypress/e2e/argo_cd.cy.js
@@ -55,7 +55,7 @@ it('allows deploying a healthy app for a new project', () => {
     cy.get('[qe-id="applications-list-button-create"]').click();
 
     cy.get('.applications-list').within(() => {
-      cy.contains(`${applications.newApplication.name}-${random}`, {timeout: 60000}).click();
+      cy.contains(`${applications.newApplication.name}-${random}`, {timeout: 60000}).click({force: true});
     });
   });
   // Ensure that UI shows the basic K8s objects
