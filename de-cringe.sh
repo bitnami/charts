2f937651532c0f4880aa7b996035493fb3c673d9
Jordan McClintock <jordan@defenseunicorns.com>
2025-01-07 12:19:26 -0500

    remove image logic and change dep refs to ghcr (#1)

---
+++ de-cringe.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+# Replace 'oci://registry-1.docker.io/bitnamicharts' with 'oci://ghcr.io/defenseunicorns/bitferno' in all 'Chart.yaml' files under the 'bitnami' folder
+find bitnami -type f -name 'Chart.yaml' -exec sed -i 's|oci://registry-1.docker.io/bitnamicharts|oci://ghcr.io/defenseunicorns/bitferno|g' {} +
+
+# Remove contents of insecureImages template if file contains "relocatedImages"
+if grep -q "relocatedImages" bitnami/common/templates/_errors.tpl; then
+  sed -i '/{{- \$relocatedImages := list -}}/,/{{- print \$warnString -}}/d' bitnami/common/templates/_errors.tpl
+  sed -i '$d' bitnami/common/templates/_errors.tpl
+fi
