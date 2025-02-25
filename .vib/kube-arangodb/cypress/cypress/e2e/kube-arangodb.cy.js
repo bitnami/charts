23bf6df24f3cfc55292a21d62b24e8cf3f0b25f9
Javier J. Salmerón García <javier.salmeron@broadcom.com>
2025-02-25 16:22:15 +0100

    test: :white_check_mark: Ensure that the state_color is green
    Signed-off-by: Javier J. Salmerón García <javier.salmeron@broadcom.com>

--- .vib/kube-arangodb/cypress/cypress/e2e/kube-arangodb.cy.js
+++ .vib/kube-arangodb/cypress/cypress/e2e/kube-arangodb.cy.js
@@ -5,10 +5,8 @@
 
 /// <reference types="cypress" />
 
-it('can access the script run and cluster status', () => {
+it('Can access the API and get the deployed Arango Database status', () => {
   let token;
-  let deploymentFound = false;
-
   cy.request({
     method: 'POST',
     url: '/login',
@@ -19,9 +17,10 @@ it('can access the script run and cluster status', () => {
   }).then((response) => {
     expect(response.status).to.eq(200);
     token = response.body.token;
-  });
-  cy.then(() => {
-    cy.wait(20000);
+  }).then(() => {
+    // In order to avoid a race condition, we wait 60 seconds to ensure that the
+    // Arango database is fully deployed and available
+    cy.wait(60000);
     cy.request({
       method: 'GET',
       url: '/api/deployment',
@@ -31,15 +30,12 @@ it('can access the script run and cluster status', () => {
     }).then((apiResponse) => {
       expect(apiResponse.status).to.eq(200);
       if (apiResponse.body.deployments[0]) {
-        deploymentFound = true;
         cy.fixture('deployments').then((d) => {
           // This ensures that the script in the job was run
           expect(apiResponse.body.deployments[0].name).to.eq(d.deployment.name);
+          expect(apiResponse.body.deployments[0].state_color).to.eq('green');
         });
       }
     });
   });
-  cy.then(() => {
-    expect(deploymentFound).to.be.true;
-  })
 });
